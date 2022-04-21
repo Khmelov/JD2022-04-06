289a569069acafa54784ce358d900d1411b0eb7c
Kameisha <xim9052@gmail.com>
2022-04-22 01:22:19 +0300

    jd01_05 Complete level A,B

--- src/by/it/kameisha/jd01_05/TaskC.java
+++ src/by/it/kameisha/jd01_05/TaskC.java
@@ -11,37 +11,30 @@ public class TaskC {
 
     private static void step1() {
         Random random = new Random();
-        int length = random.nextInt(20,31);
-        double[] array =  new double[length];
+        int length = random.nextInt(20, 31);
+        double[] array = new double[length];
         System.out.println("Массив A[]");
         double randomDouble;
-        for (int i = 0; i < array.length ; i++) {
-           randomDouble=0;
-           while (randomDouble<5.33 || randomDouble>9) {
-               randomDouble = random.nextDouble(5.33, 9.01);
-           }
-           array[i] = cbrt(pow(randomDouble,2)+4.5);
-           System.out.printf("%1c[ %-2d] = %8.5f    ",'A',i,array[i]);
-           if((i+1)%5==0 || (i+1)==array.length){
-               System.out.println();
-           }
-       }
-        System.out.println();
-       int count = 0;
-       boolean[] elementsDelete = new boolean[array.length];
         for (int i = 0; i < array.length; i++) {
-            if(array[i] > 3.5){
-                elementsDelete[i] = false;
+            randomDouble = 0;
+            while (randomDouble < 5.33 || randomDouble > 9) {
+                randomDouble = random.nextDouble(5.33, 9.01);
+            }
+            array[i] = cbrt(pow(randomDouble, 2) + 4.5);
+            printArray('A',i,array);
+        }
+        System.out.println();
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 3.5) {
                 count++;
-            } else {
-                elementsDelete[i] = true;
             }
         }
         double[] newArray = new double[count];
         count = 0;
-        for (int i = 0; i < array.length; i++) {
-            if(elementsDelete[i]==false){
-                newArray[count] = array[i];
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > 3.5) {
+                newArray[count] = array[j];
                 count++;
             }
         }
@@ -50,13 +43,16 @@ public class TaskC {
         System.out.println("Массив B[] из элементов массива A > 3.5");
         for (int i = 0; i < newArray.length; i++) {
             temporary = temporary + newArray[i];
-            System.out.printf("%1c[ %-2d] = %8.5f    ",'B',i,newArray[i]);
-            if(i+1== newArray.length || (i+1)%5==0){
-                System.out.println();
-            }
+            printArray('B',i,newArray);
         }
-        result = pow(temporary,1.0/newArray.length);
+        result = pow(temporary, 1.0 / newArray.length);
         System.out.println(result);
     }
-
+    private static void printArray (char A, int i , double[] array) {
+        System.out.printf("%1c[ %-2d] = %8.5f    ", A, i, array[i]);
+        if (i + 1 == array.length || (i + 1) % 5 == 0) {
+            System.out.println();
+        }
+    }
 }
+
