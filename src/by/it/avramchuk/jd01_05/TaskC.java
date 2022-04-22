b26fe7bd3d909a70578e6aae4b4e5245cb02c4ac
Аврамчук Сергей <dvizenets@mail.ru>
2022-04-22 18:20:10 +0300

    jd01_05 C2 completed

--- src/by/it/avramchuk/jd01_05/TaskC.java
+++ src/by/it/avramchuk/jd01_05/TaskC.java
@@ -1,7 +1,25 @@
 package by.it.avramchuk.jd01_05;
 
+import java.util.Arrays;
+
 public class TaskC {
     public static void main(String[] args) {
+
+       step1();
+
+    // level C2
+       int[] arrayA = makeArrayA();
+       int[] arrayB = makeArrayB(arrayA);
+       Arrays.sort(arrayB);
+        System.out.println("Массив А:");
+       printArray(arrayA, "A");
+        System.out.println();
+        System.out.println("Массив В:");
+       printArray(arrayB, "B");
+        System.out.println();
+    }
+
+    private static void step1() {
         double start = 5.33;
         double stop = 9.0;
         int n = ((int) (Math.random()*21))+20;
@@ -39,7 +57,102 @@ public class TaskC {
             pr = pr*element;
         }
         average = Math.pow(pr, 1.0/miniArray.length);
+        System.out.println();
         System.out.println("Average = "+average);
 
+    }
+
+    public static int[] makeArrayA(){
+
+        int [] arrayA = new int[31];
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayA[i] = ((int) (Math.random()*348))+103;
+
+        }
+        return arrayA;
+    }
+
+    public static int[] makeArrayB(int[] array){
+        int counter=0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]*0.1>i){
+                counter++;
+            }
+        }
+        int[] arrayB = new int[counter];
+        counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]*0.1>i){
+                arrayB[counter]= array[i];
+                counter++;
+            }
+        }
+        return arrayB;
+    }
+
+    public static void printArray(int[] array, String name){
+        int rows=0;
+        int cols=0;
+        int n=5;
+        while(n>=2){
+            if(array.length%n==0){
+                cols=n;
+                rows = array.length/n;
+                break;
+            } else if((array.length+1)%n==0){
+                cols= n;
+                rows = (array.length+1)/n;
+                break;
+            }
+            n--;
+        }
+        int rowPoint= 1;
+        int colPoint = 0;
+
+        while (colPoint<=cols){
+            if(colPoint==0){
+                System.out.print("\u2554"+"\u2550".repeat(9));
+            }else if(colPoint<cols){
+                System.out.print("\u2566"+"\u2550".repeat(9));
+            }else{
+                System.out.print("\u2557");
+            }
+            colPoint++;
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            colPoint=0;
+            System.out.printf("\u2551%1s[%2d]=%3d",name, i, array[i]);
+            if((i+1)%cols==0){
+                System.out.println("\u2551");
+                if (i!=array.length-1){
+                    while (colPoint<=cols){
+                        if(colPoint==0){
+                            System.out.print("\u2560"+"\u2550".repeat(9));
+                        }else if(colPoint<cols){
+                            System.out.print("\u256C"+"\u2550".repeat(9));
+                        }else{
+                            System.out.print("\u2563");
+                        }
+                        colPoint++;
+                    }
+                    System.out.println();
+                }
+            }
+            if (i==array.length-1 && array.length<rows*cols){
+                System.out.println("\u2551         \u2551");
+            }
+        }
+        colPoint=0;
+        while (colPoint<=cols){
+            if(colPoint==0){
+                System.out.print("\u255A"+"\u2550".repeat(9));
+            }else if(colPoint<cols){
+                System.out.print("\u2569"+"\u2550".repeat(9));
+            }else{
+                System.out.print("\u255D");
+            }
+            colPoint++;
+        }
     }
 }
