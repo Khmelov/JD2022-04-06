5274a8848da5b55e68f75a76a40c164b37124534
Kadulin M <kadulinmihail@gmail.com>
2022-04-23 00:03:54 +0200

    jd01-05 Task C2

--- src/by/it/kadulin/jd01_05/TaskC.java
+++ src/by/it/kadulin/jd01_05/TaskC.java
@@ -2,10 +2,12 @@ package by.it.kadulin.jd01_05;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TaskC {
     public static void main(String[] args) {
-        step1();
+//        step1();
+        step2();
     }
 
     private static void step1() {
@@ -38,4 +40,82 @@ public class TaskC {
         }
         System.out.printf("%nСреднее геометрическое чисел = %.5f", Math.pow(mlt, 1.0 / countNumbers));
     }
+
+    private static void step2() {
+        int[] arrayA = new int[31];
+        ArrayList<Integer> arrayB = new ArrayList<>();
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayA[i] = rnd();
+            if (arrayA[i] / 10 > i) {
+                arrayB.add(arrayA[i]);
+            }
+        }
+        arrayB.sort((Integer::compareTo));
+        String firstA  = "╔===========╦===========╦===========╦===========╦===========╗";
+        String middleA = "╠===========╬===========╬===========╬===========╬===========╣";
+        String lastA   = "╚===========╩===========╩===========╩===========╩===========╝";
+        String firstB  = "╔===========╦===========╗";
+        String middleB = "╠===========╬===========╣";
+        String lastB   = "╚===========╩===========╝";
+
+        for (int i = 1; i <= arrayA.length; i++) {
+            if (i == 1) {
+                System.out.println(firstA);
+            }
+            System.out.printf("║ %s[%2d]=%3d ", "A", i - 1, arrayA[i - 1]); // 1 + 1 + 9 + 1 + 1
+            if (i % 5 == 0) {
+                System.out.println("║");
+                System.out.println(middleA);
+            }
+            if (i == arrayA.length) {
+                System.out.println("║           ║           ║           ║           ║");
+                System.out.println(lastA);
+            }
+        }
+        System.out.println();
+//        System.out.println(arrayB.size());
+//        System.out.println(arrayB.toString());
+        int countFullRows = arrayB.size() / 5;
+        int j = 0;
+        int remain = arrayB.size() % 5;
+        for (int i = 0; i < countFullRows; i++) {
+            if (i == 0) {
+                System.out.println(firstA);
+            }
+            System.out.printf("║ %s[%2d]=%3d ║ %s[%2d]=%3d ║ %s[%2d]=%3d ║ %s[%2d]=%3d ║ %s[%2d]=%3d ║%n",
+                    "B", j, arrayB.get(j), "B", j + 1, arrayB.get(j + 1), "B", j + 2, arrayB.get(j + 2), "B", j + 3,
+                    arrayB.get(j + 3), "B", j + 4, arrayB.get(j + 4));
+            if (i != countFullRows - 1) {
+                System.out.println(middleA);
+            }
+
+            j = j + 5;
+        }
+        if (remain == 0) {
+            System.out.println(lastA);
+        }
+        else {
+            System.out.println(middleA);
+            for (int i = countFullRows * 5; i < arrayB.size(); i++) {
+                System.out.printf("║ %s[%2d]=%3d ", "B", i, arrayB.get(i));
+            }
+            for (int i = 0; i < 5 - remain; i++) {
+                System.out.printf("║           ");
+            }
+            System.out.print("║\n");
+            System.out.println(lastA);
+        }
+
+
+
+    }
+
+    private static int rnd() {
+        return 103 + (int) (Math.random() * (347 + 1));
+    }
 }
+/*
+╔===========╦
+║ A[ 1]=333 ║
+╠===========╬
+ */
