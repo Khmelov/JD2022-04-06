1cd10920295bf475236f50398edd5d11f2a8591b
Dzmitry_Varabyeu <dzmitry_varabyeu@epam.com>
2022-04-22 22:08:41 +0300

    jd01_04 Update task B1

--- src/by/it/varabyeu/jd01_04/TaskB.java
+++ src/by/it/varabyeu/jd01_04/TaskB.java
@@ -6,6 +6,8 @@ public class TaskB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+
+
         String[] lastName = new String[n];
         for (int i = 0; i < lastName.length; i++) {
             lastName[i] = scanner.next();
@@ -18,5 +20,32 @@ public class TaskB {
                 salary[i][j] = scanner.nextInt();
             }
         }
+        int[] total = new int[n];
+        for (int i = 0; i < salary.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < 4; j++) {
+                sum = sum + salary[i][j];
+            }
+            total[i] = sum;
+        }
+
+        System.out.println("-".repeat(50));
+        System.out.printf("%-9s%-9s%-9s%-9s%-9s%-5s\n", "Фамилия", "Квартал1", "Квартал2", "Квартал3", "Квартал4", "Итого");
+        System.out.println("-".repeat(50));
+
+        for (int i = 0; i < lastName.length; i++) {
+            System.out.printf("%-10s", lastName[i]+" :");
+
+            for (int j = 0; j < salary[i].length; j++) {
+                System.out.printf("%-10s", salary[i][j]);
+            }
+
+            for (int k = 0; k < total.length; k++) {
+                System.out.printf("%-10s", total[i]);
+                break;
+            }
+            System.out.println();
+        }
+        System.out.println("-".repeat(50));
     }
 }
