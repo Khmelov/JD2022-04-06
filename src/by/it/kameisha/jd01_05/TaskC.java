b5701a1f2233465431128f53241f83e0f1ed2aec
Kameisha <xim9052@gmail.com>
2022-04-22 10:59:19 +0300

    jd01_05 Complete level A,B

--- src/by/it/kameisha/jd01_05/TaskC.java
+++ src/by/it/kameisha/jd01_05/TaskC.java
@@ -7,21 +7,22 @@ import static java.lang.Math.*;
 public class TaskC {
     public static void main(String[] args) {
         step1();
+        step2();
     }
 
     private static void step1() {
-        Random random = new Random();
-        int length = random.nextInt(20, 31);
+        Random randomX = new Random();
+        int length = randomX.nextInt(20, 41);
         double[] array = new double[length];
         System.out.println("Массив A[]");
         double randomDouble;
         for (int i = 0; i < array.length; i++) {
             randomDouble = 0;
             while (randomDouble < 5.33 || randomDouble > 9) {
-                randomDouble = random.nextDouble(5.33, 9.01);
+                randomDouble = randomX.nextDouble(5.33, 9.01);
             }
             array[i] = cbrt(pow(randomDouble, 2) + 4.5);
-            printArray('A',i,array);
+            printArray('A', i, array);
         }
         System.out.println();
         int count = 0;
@@ -38,21 +39,68 @@ public class TaskC {
                 count++;
             }
         }
-        double temporary = 0;
-        double result;
+        double result = 0;
         System.out.println("Массив B[] из элементов массива A > 3.5");
         for (int i = 0; i < newArray.length; i++) {
-            temporary = temporary + newArray[i];
-            printArray('B',i,newArray);
+            result = result + newArray[i];
+            printArray('B', i, newArray);
         }
-        result = pow(temporary, 1.0 / newArray.length);
-        System.out.println(result);
+        result = pow(result, 1.0 / newArray.length);
+        System.out.printf("Среднее геометрическое массива B = %-8.5f%n", result);
     }
-    private static void printArray (char A, int i , double[] array) {
+
+    private static void printArray(char A, int i, double[] array) {
         System.out.printf("%1c[ %-2d] =%8.5f    ", A, i, array[i]);
         if (i + 1 == array.length || (i + 1) % 5 == 0) {
             System.out.println();
         }
     }
+
+    private static void step2() {
+        Random randomElement = new Random();
+        int[] array = new int[31];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = randomElement.nextInt(103, 450);
+        }
+        int[] newArray = getNewArray(array);
+
+    }
+
+    private static int[] getNewArray(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((double) array[i] * 0.1 > i) {
+                count++;
+            }
+        }
+        int[] newArray = new int[count];
+        count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((double) array[i] * 0.1 > i) {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+        sort(newArray);
+        return newArray;
+    }
+    private static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minElement = array[i];
+            int minElementIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (minElement > array[j]) {
+                    minElement = array[j];
+                    minElementIndex = j;
+                }
+            }
+            if (i != minElementIndex) {
+                int temporary = array[i];
+                array[i] = array[minElementIndex];
+                array[minElementIndex] = temporary;
+            }
+        }
+    }
 }
+
 
