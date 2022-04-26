d1dc1cc8baf39b285da4a725da49c46413dfa7ff
Kadulin M <kadulinmihail@gmail.com>
2022-04-26 12:01:17 +0200

    jd01-06 Tasks С2 in process upd4

--- src/by/it/kadulin/jd01_06/TaskC2.java
+++ src/by/it/kadulin/jd01_06/TaskC2.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskC2 {
-    public static final int seed = 100;
+    public static final int seed = 300;
     public static void main(String[] args) {
 
         long startSlow = System.nanoTime();
@@ -18,60 +18,59 @@ public class TaskC2 {
         String fastText = fast(Poem.text);
         System.out.println(fastText);
         System.out.printf("%s %d%n", "fast", (System.nanoTime() - startFast) / 1_000_000);
+//        System.out.println(slowText.equals(fastText));
     }
 
     static String slow(String text) {
-        String[] words = poemToArray(text);
+//        String[] words = poemToArray(text);
+        String[] words = text.split("[^А-Яа-яЁё]+");
         String result = "";
         int length = 0;
-        Random random = new Random();
-        random.setSeed(seed);
-        while (length < 100_000) {
+        Random random = new Random(seed);
+        while (result.length() < 100_000) {
             int indexWordArray = random.nextInt(words.length);
             result = result.concat(words[indexWordArray]).concat(" ");
-            length += result.length();
+//            length += result.length();
         }
         return result;
     }
 
     static String fast(String text) {
-        String[] words = poemToArray(text);
+//        String[] words = poemToArray(text);
+        String[] words = text.split("[^А-Яа-яЁё]+");
         StringBuilder sb = new StringBuilder();
-        Random random = new Random();
-        random.setSeed(seed);
+        Random random = new Random(seed);
         int length = 0;
-        while (length < 100_000) {
+        while (sb.length() < 100_000) {
             int indexWordArray = random.nextInt(words.length);
-//            System.out.print(indexWordArray + " ");
             sb.append(words[indexWordArray]).append(" ");
-            length += sb.length();
+//            length += sb.length();
         }
         return sb.toString();
     }
 
-    private static String[] poemToArray(String text) {
-        Pattern pattern = Pattern.compile("[А-Яа-яЁё]+");
-        Matcher matcher = pattern.matcher(text);
-        String[] words = {};
-        while (matcher.find()) {
-            if (words.length != 0) {
-                boolean flag = false;
-                for (int i = 0; i < words.length; i++) {
-                    if (words[i].equals(matcher.group())) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag) {
-                    continue;
-                }
-            }
-            words = Arrays.copyOf(words, words.length + 1);
-            words[words.length - 1] = matcher.group();
-        }
-//        System.out.println(words.length);
-        return words;
-    }
+//    private static String[] poemToArray(String text) {
+//        Pattern pattern = Pattern.compile("[А-Яа-яЁё]+");
+//        Matcher matcher = pattern.matcher(text);
+//        String[] words = {};
+//        while (matcher.find()) {
+//            if (words.length != 0) {
+//                boolean flag = false;
+//                for (int i = 0; i < words.length; i++) {
+//                    if (words[i].equals(matcher.group())) {
+//                        flag = true;
+//                        break;
+//                    }
+//                }
+//                if (flag) {
+//                    continue;
+//                }
+//            }
+//            words = Arrays.copyOf(words, words.length + 1);
+//            words[words.length - 1] = matcher.group();
+//        }
+//        return words;
+//    }
 
 
 }
