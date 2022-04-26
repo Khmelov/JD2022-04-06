4b968ef8871eea2ef21cc803ab5b46c3f71d5aff
korennaya <kgv203@gmail.com>
2022-04-26 16:39:43 +0300

    Task JD01_06 A1_A2 completed

---
+++ src/by/it/korennaya/jd01_06/TaskA2.java
@@ -0,0 +1,40 @@
+package by.it.korennaya.jd01_06;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA2 {
+
+    public static final String WORD_PATTERN = "[а-яА-ЯёЁ]+";
+    private static int[]counts = {};
+    private static String [] words = {};
+
+    public static void main(String[] args){
+        Pattern pattern = Pattern.compile(WORD_PATTERN);
+        Matcher matcher = pattern.matcher (Poem.text);
+        while(matcher.find()) {
+            String word = matcher.group();
+            process (word);
+        }
+        printResult();
+    }
+
+    private static void process(String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (word.equals (words [i])){
+                counts[i]++;
+                return;
+            }
+        }
+        words = Arrays.copyOf(words,words.length+1);
+        words[words.length-1]=word;
+        counts= Arrays.copyOf(counts,counts.length+1);
+        counts[counts.length-1]=1;
+    }
+    private static void printResult() {
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("%s=%d%n", words[i], counts[i]);
+        }
+    }
+}
