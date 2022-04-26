4d2d3be652fd957b64db84c975b8be632a0b0104
Arseni Hlaz <mafusaik@gmail.com>
2022-04-26 19:14:14 +0300

    jd01-06 add completed only TaskC2

---
+++ src/by/it/arsenihlaz/jd01_06/TaskC1.java
@@ -0,0 +1,37 @@
+package by.it.arsenihlaz.jd01_06;
+/*
+Разделить текст по строкам
+узнать длину максимальной строки
+искать пробельные группы и добавлять пробел
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskC1 {
+    public static void main(String[] args) {
+//        String sentences[] = Poem.text.split("[\n]");
+//        Pattern pattern = Pattern.compile("[\\s+]");
+//        int maxLenght = 0;
+//        for (int i = 0; i < sentences.length; i++) {
+//            if (sentences[i].length() > maxLenght) {
+//                maxLenght = sentences[i].length();
+//            }
+//            //System.out.println(sentences[i]);
+//        }
+//        for (int i = 0; i < sentences.length; i++) {
+//            while (sentences[i].length() < maxLenght) {
+//                StringBuilder changeSentences = new StringBuilder(sentences[i]);
+//
+//                Matcher matcher = pattern.matcher(changeSentences);
+//                while (matcher.find()) {
+////                    changeSentences.insert(matcher.start(), " ");
+////                    changeSentences.insert(matcher.end(), " ");
+//               // changeSentences=matcher.replaceAll("  ");
+//                    System.out.println(changeSentences);
+//                   // break;
+//                }
+//            }
+//        }
+    }
+}
