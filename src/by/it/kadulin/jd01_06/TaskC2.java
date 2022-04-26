4a204cf5ceac9beb129c6f0ea5c1593c2b111e3a
Kadulin M <kadulinmihail@gmail.com>
2022-04-26 12:02:16 +0200

    jd01-06 Tasks С2 Complete

--- src/by/it/kadulin/jd01_06/TaskC2.java
+++ src/by/it/kadulin/jd01_06/TaskC2.java
@@ -18,59 +18,27 @@ public class TaskC2 {
         String fastText = fast(Poem.text);
         System.out.println(fastText);
         System.out.printf("%s %d%n", "fast", (System.nanoTime() - startFast) / 1_000_000);
-//        System.out.println(slowText.equals(fastText));
     }
 
     static String slow(String text) {
-//        String[] words = poemToArray(text);
         String[] words = text.split("[^А-Яа-яЁё]+");
         String result = "";
-        int length = 0;
         Random random = new Random(seed);
         while (result.length() < 100_000) {
             int indexWordArray = random.nextInt(words.length);
             result = result.concat(words[indexWordArray]).concat(" ");
-//            length += result.length();
         }
         return result;
     }
 
     static String fast(String text) {
-//        String[] words = poemToArray(text);
         String[] words = text.split("[^А-Яа-яЁё]+");
         StringBuilder sb = new StringBuilder();
         Random random = new Random(seed);
-        int length = 0;
         while (sb.length() < 100_000) {
             int indexWordArray = random.nextInt(words.length);
             sb.append(words[indexWordArray]).append(" ");
-//            length += sb.length();
         }
         return sb.toString();
     }
-
-//    private static String[] poemToArray(String text) {
-//        Pattern pattern = Pattern.compile("[А-Яа-яЁё]+");
-//        Matcher matcher = pattern.matcher(text);
-//        String[] words = {};
-//        while (matcher.find()) {
-//            if (words.length != 0) {
-//                boolean flag = false;
-//                for (int i = 0; i < words.length; i++) {
-//                    if (words[i].equals(matcher.group())) {
-//                        flag = true;
-//                        break;
-//                    }
-//                }
-//                if (flag) {
-//                    continue;
-//                }
-//            }
-//            words = Arrays.copyOf(words, words.length + 1);
-//            words[words.length - 1] = matcher.group();
-//        }
-//        return words;
-//    }
-
-
 }
