205424bdf02f454bf1a6bb17a3b1348a68f91c61
Oksana Ragach <oksanaragach@gmail.com>
2022-04-27 17:17:16 +0300

    jd01-06 TaskC1 find maxline

--- src/by/it/ragach/jd01_06/TaskC1.java
+++ src/by/it/ragach/jd01_06/TaskC1.java
@@ -1,5 +1,34 @@
 package by.it.ragach.jd01_06;
 
 public class TaskC1 {
-    
+    public static void main(String[] args) {
+        String text = Poem.text;
+        String [] sentences = text.split("[\n]");
+
+
+        int maxline = 0;
+
+        for (String sentence : sentences) {
+            if (maxline<sentence.length()) {
+                maxline = sentence.length();
+            }
+
+        }
+        System.out.println(maxline);
+
+
+        }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
