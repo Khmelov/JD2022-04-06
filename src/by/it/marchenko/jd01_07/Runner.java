60bd3316870e2844441e4150440a66c90bc29be2
Delarez <92412420+Delarez@users.noreply.github.com>
2022-04-28 00:10:49 +0300

    jd01_07 Task A complete

---
+++ src/by/it/marchenko/jd01_07/Runner.java
@@ -0,0 +1,17 @@
+package by.it.marchenko.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var myVar1 = new Scalar(3.14);
+        System.out.println(myVar1);
+        Var myVar2 = new Scalar("2.15");
+        System.out.println(myVar2);
+        Scalar myVar3 = new Scalar("2.158");
+        System.out.println(myVar3);
+        Scalar myVar4 = new Scalar(myVar3);
+        System.out.println(myVar4);
+        double d = myVar4.getValue();
+
+
+    }
+}
