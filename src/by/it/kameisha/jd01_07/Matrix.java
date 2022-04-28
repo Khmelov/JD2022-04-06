16c283ff6fb117f9b524fd50dc29d0e26b00031f
Kameisha <xim9052@gmail.com>
2022-04-28 22:33:31 +0300

    jd01_07 start Task C

--- src/by/it/kameisha/jd01_07/Matrix.java
+++ src/by/it/kameisha/jd01_07/Matrix.java
@@ -1,5 +1,7 @@
 package by.it.kameisha.jd01_07;
 
+import java.util.Arrays;
+
 public class Matrix extends Var {
 
     private double[][] value;
@@ -18,5 +20,29 @@ public class Matrix extends Var {
             array[i] = matrix.value[i].clone();
         }
         this.value = array;
+    }
+
+    public Matrix(String strMatrix){
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder strValue = new StringBuilder("{");;
+        for (int i = 0; i < value.length; i++) {
+            String splitter = "";
+            strValue.append("{");
+            for (int j = 0; j < value[i].length; j++) {
+                strValue.append(splitter).append(value[i][j]);
+                splitter = ", ";
+            }
+            strValue.append("}");
+            if(i == value.length-1){
+                splitter = "";
+            }
+            strValue.append(splitter);
+        }
+        strValue.append("}");
+        return strValue.toString();
     }
 }
