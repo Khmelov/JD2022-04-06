f1fb1d5a5aff421b86beab472b2bcd590d4896ce
Kameisha <xim9052@gmail.com>
2022-04-28 21:27:44 +0300

    jd01_07 complete Task B

--- src/by/it/kameisha/jd01_07/Runner.java
+++ src/by/it/kameisha/jd01_07/Runner.java
@@ -2,6 +2,14 @@ package by.it.kameisha.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-
+        String strVector = "{1.0, 2.0, 4.0}";
+        strVector = strVector.replace("{","");
+        strVector = strVector.replace("}","");
+        String[] strings = strVector.split(",");
+        double[] value = new double[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            value[i] = Double.parseDouble(strings[i]);
+        }
+        System.out.println(value[2]);
     }
 }
