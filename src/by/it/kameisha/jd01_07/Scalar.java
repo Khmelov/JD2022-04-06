d0c9e7b2568b6bde24652a3665ee938bbbfa2d8e
Kameisha <xim9052@gmail.com>
2022-04-28 20:31:49 +0300

    jd01_07 complete Task A

--- src/by/it/kameisha/jd01_07/Scalar.java
+++ src/by/it/kameisha/jd01_07/Scalar.java
@@ -1,4 +1,21 @@
 package by.it.kameisha.jd01_07;
 
-public class Scalar {
+public class Scalar extends Var {
+    private double value;
+    public Scalar(double value){
+        this.value = value;
+    }
+
+    public Scalar(Scalar scalar){
+        this.value = scalar.value;
+    }
+
+    public Scalar(String strScalar){
+        this.value = Double.parseDouble(strScalar);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
 }
