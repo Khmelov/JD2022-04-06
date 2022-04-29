abfd0acbea83688e6e466eec28f6e047d3a2a12f
Aleksandr Machuga <machuga.aleksandr@gmail.com>
2022-04-30 00:28:27 +0300

    completed 01-07 C

--- src/by/it/machuga/jd01_07/Converter.java
+++ src/by/it/machuga/jd01_07/Converter.java
@@ -1,5 +1,9 @@
 package by.it.machuga.jd01_07;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Converter {
     static double[] convertStringToDoubleArray(String strVector) {
         String formattedString = strVector.replaceAll("[{}]", "");
@@ -22,5 +26,32 @@ public class Converter {
             }
         }
         return stringBuilder.toString();
+    }
+
+    static String convertDoubleMultiDimensionArrayToString(double[][] array) {
+        StringBuilder stringBuilder = new StringBuilder("{");
+        for (int i = 0; i < array.length; i++) {
+            stringBuilder.append(convertDoubleArrayToString(array[i]));
+            if (i != array.length - 1) {
+                stringBuilder.append(", ");
+            } else {
+                stringBuilder.append("}");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    static double[][] convertStringToDoubleMultiDimensionArray(String strMatrix) {
+        Pattern pattern = Pattern.compile("\\{ ?(\\d+.\\d+,? ?)+}");
+        Matcher matcher = pattern.matcher(strMatrix);
+        double[][] arrayMultiDimensionArray = new double[0][];
+        while (matcher.find()) {
+            String stringArray = matcher.group();
+            double[] element = convertStringToDoubleArray(stringArray);
+            double[][] temp = Arrays.copyOf(arrayMultiDimensionArray, arrayMultiDimensionArray.length + 1);
+            temp[temp.length - 1] = element;
+            arrayMultiDimensionArray = temp;
+        }
+        return arrayMultiDimensionArray;
     }
 }
