f73bdad2b0e8181421427cab5543b1e20a1b407f
kudelkoartem <sinergia86@yandex.ru>
2022-04-29 16:33:33 +0300

    jd01-07 TaskB1 started

---
+++ src/by/it/kudelko/jd01_07/Runner.java
@@ -0,0 +1,13 @@
+package by.it.kudelko.jd01_07;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Scalar var1 = new Scalar(3.1415);
+        Scalar var3 = new Scalar("3.1415");
+        System.out.println(var1);
+        System.out.println(var3);
+
+
+    }
+}
