705ec818a247cd01ec55dc7d80047c67a824860f
Kadulin M <kadulinmihail@gmail.com>
2022-04-30 16:00:52 +0200

    jd01-08 Complete A, B

--- src/by/it/kadulin/jd01_08/Vector.java
+++ src/by/it/kadulin/jd01_08/Vector.java
@@ -26,22 +26,65 @@ class Vector extends Var {
             for (int i = 0; i < result.length; i++) {
                 result[i] = result[i] + scalar.getValue();
             }
+            return new Vector(result);
+        }
+        else if (other instanceof Vector vector) {
+            double[] result = values.clone();
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] + vector.values[i];
+            }
+            return new Vector(result);
         }
         return super.add(other);
     }
 
     @Override
     public Var sub(Var other) {
+        if (other instanceof Scalar scalar) {
+            double[] result = values.clone();
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] - scalar.getValue();
+            }
+            return new Vector(result);
+        }
+        else if (other instanceof Vector vector) {
+            double[] result = values.clone();
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] - vector.values[i];
+            }
+            return new Vector(result);
+        }
         return super.sub(other);
     }
 
     @Override
     public Var mul(Var other) {
+        if (other instanceof Scalar scalar) {
+            double[] result = values.clone();
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] * scalar.getValue();
+            }
+            return new Vector(result);
+        }
+        else if (other instanceof Vector vector) {
+            double result = 0;
+            for (int i = 0; i < vector.values.length; i++) {
+                result = result + (vector.values[i] * this.values[i]);
+            }
+            return new Scalar(result);
+        }
         return super.mul(other);
     }
 
     @Override
     public Var div(Var other) {
+        if (other instanceof Scalar scalar) {
+            double[] result = values.clone();
+            for (int i = 0; i < result.length; i++) {
+                result[i] = result[i] / scalar.getValue();
+            }
+            return new Vector(result);
+        }
         return super.div(other);
     }
 
