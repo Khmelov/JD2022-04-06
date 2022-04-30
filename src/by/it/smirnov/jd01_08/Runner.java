37da8fb84129583e579a54f868fdf19fcd1b1d5c
Anton Smirnov <watashinodenwadesu@gmail.com>
2022-05-01 01:34:04 +0300

    jd01-08 taskA complete.

--- src/by/it/smirnov/jd01_08/Runner.java
+++ src/by/it/smirnov/jd01_08/Runner.java
@@ -1,15 +1,26 @@
 package by.it.smirnov.jd01_08;
 
 public class Runner {
-
+    private static void print(Var var) {System.out.println(var);}
     public static void main(String[] args) {
-        Var v1 = new Scalar(3.1415);
-        String line = "{1,2,4}";
-        String matrixLine = "{{1.0, 2.0}, {3.0, 4.0}}";
-        double[] vector = {1.0, 2.0, 4.0};
-        Var v2 = new Vector(line);
-        Var v3 = new Matrix(matrixLine);
-        System.out.println(v3);
-
+        Var s = new Scalar(3.0);
+        Var v = new Vector(new double[]{1,2,3}); // закомментируйте вектор и/или
+        Var m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}");// матрицу, если вы их не реализовали
+        /* Уровень сложности A (калькулятор) */
+        print(s.add(s)); //выведет в консоль 6.0
+        print(s.sub(s)); //выведет в консоль 0.0
+        print(s.mul(s)); //выведет в консоль 9.0
+        print(s.div(s)); //выведет в консоль 1.0
+        /* Уровень сложности B (векторные операции) закомментируйте, если не реализовали */
+        print(v.add(v)); //выведет в консоль {2.0, 4.0, 6.0}
+        print(v.sub(v)); //выведет в консоль {0.0, 0.0, 0.0}
+        print(v.mul(v)); //выведет в консоль 14.0
+        print(v.div(v)); //сообщит о невозможности операции
+/* Уровень сложности C (матричные операции и умножение на вектор)
+закомментируйте, если не реализовали */
+        print(m.add(m)); //{{2.0, 4.0, 6.0}, {8.0, 10.0, 12.0}, {14.0, 16.0, 18.0}}
+        print(m.sub(m)); //{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}
+        print(m.mul(m)); //{{30.0, 36.0, 42.0}, {66.0, 81.0, 96.0}, {102.0, 126.0, 150.0}}
+        print(m.mul(v)); //{14.0, 32.0, 50.0}
     }
 }
--- src/by/it/smirnov/jd01_08/Scalar.java
+++ src/by/it/smirnov/jd01_08/Scalar.java
@@ -20,4 +20,40 @@ private final double value;
     public String toString() {
         return Double.toString(value);
     }
+
+    @Override
+    public Var add(Var other) {
+        if(other instanceof Scalar) {
+            double add = this.value + ((Scalar) other).value;
+            return new Scalar(add);
+        }
+        else return other.add(this);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if(other instanceof Scalar) {
+            double sub = this.value - ((Scalar) other).value;
+            return new Scalar(sub);
+        }
+        else return new Scalar(-1).mul(other).add(this);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if(other instanceof Scalar) {
+            double mul = this.value * ((Scalar) other).value;
+            return new Scalar(mul);
+        }
+        else return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if(other instanceof Scalar) {
+            double div = this.value / ((Scalar) other).value;
+            return new Scalar(div);
+        }
+        else return super.div(other);
+    }
 }
