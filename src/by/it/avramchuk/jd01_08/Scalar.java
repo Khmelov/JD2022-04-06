204d98925b3db948471e4a665bb3ac611a3446be
Аврамчук Сергей <dvizenets@mail.ru>
2022-04-30 17:41:13 +0300

    jd01_08 level A complete

--- src/by/it/avramchuk/jd01_08/Scalar.java
+++ src/by/it/avramchuk/jd01_08/Scalar.java
@@ -20,9 +20,44 @@ class Scalar extends Var {
         return value;
     }
 
-
     @Override
     public String toString() {
         return Double.toString(value);
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value + otherScalar.value);
+        } else {
+            return other.add(this);
+        }
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value * otherScalar.value);
+        } else {
+            return other.mul(this);
+        }
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value - otherScalar.value);
+        } else {
+            return other.sub(this).mul(new Scalar(-1));
+        }
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value / otherScalar.value);
+        } else {
+            return super.div(other);
+        }
     }
 }
