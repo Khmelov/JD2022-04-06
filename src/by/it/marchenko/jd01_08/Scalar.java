d60caae6dca395a982cc47e33f3f766270269303
Delarez <92412420+Delarez@users.noreply.github.com>
2022-05-02 23:19:36 +0300

    jd01_08 Add interface Operation; Add default operation for Var Class Add Runner

---
+++ src/by/it/marchenko/jd01_08/Scalar.java
@@ -0,0 +1,26 @@
+package by.it.marchenko.jd01_08;
+
+public class Scalar extends Var {
+    private final double value;
+
+    public Scalar(double value) {
+        this.value = value;
+    }
+
+    public Scalar(Scalar value) {
+        this.value = value.value;
+    }
+
+    public Scalar(String stringScalar) {
+        this.value = Double.parseDouble(stringScalar);
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+}
