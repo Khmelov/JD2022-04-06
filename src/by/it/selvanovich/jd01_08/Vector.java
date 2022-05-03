47fabb887f7a42f8d5a8fe5429968ccfd840ac89
Danila Selvanovich <lineconnor@yandex.ru>
2022-05-03 17:13:02 +0300

    jd01-08 Complete A

---
+++ src/by/it/selvanovich/jd01_08/Vector.java
@@ -0,0 +1,34 @@
+package by.it.selvanovich.jd01_08;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+    private final double[] values;
+
+    Vector(double[] values) {
+        this.values = values.clone();
+    }
+
+    public Vector(Vector vector) {
+        this.values = vector.values;
+    }
+
+    public Vector(String line) {
+        line = line.replace("{", "");
+        line = line.replace("}", "");
+        String[] strings = line.split(", ");
+        this.values = new double[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            this.values[i] = Double.parseDouble(strings[i]);
+        }
+    }
+
+    public double[] getValues() {
+        return values.clone();
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(values).replaceAll("\\[", "{").replaceAll("]", "}");
+    }
+}
