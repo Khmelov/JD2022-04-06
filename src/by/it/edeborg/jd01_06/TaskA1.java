5c335526454b4fc04b66b20656dabd8478bd47a9
Egor Edeborg <lexusxxxrock@gmail.com>
2022-05-04 18:45:44 +0300

    jd-01-05 Correct import

---
+++ src/by/it/edeborg/jd01_06/TaskA1.java
@@ -0,0 +1,22 @@
+package by.it.edeborg.jd01_06;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA1 {
+    public static void main(String[] args){
+        StringBuilder dynText = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{4,}");
+        Matcher matcher = pattern.matcher(dynText);
+        while(matcher.find()) {
+            int position = matcher.start();
+            dynText.setCharAt(position + 3, '#');
+            int length = matcher.group().length();
+            if (length > 6) {
+                dynText.setCharAt(position+6,'#')   ;
+            }
+        }
+        System.out.println(dynText);
+    }
+}
