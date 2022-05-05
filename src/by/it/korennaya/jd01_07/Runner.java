fbd7bbf424e2e0e27ff0a005841a738b95f0c54c
korennaya <kgv203@gmail.com>
2022-05-05 16:03:42 +0300

    Task JD01_08 not started

--- src/by/it/korennaya/jd01_07/Runner.java
+++ src/by/it/korennaya/jd01_07/Runner.java
@@ -2,7 +2,7 @@ package by.it.korennaya.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-        Var scalar = new Scalar(12.4);
+        Var scalar = new Scalar("3.1415");
         System.out.println(scalar);
 
         double[] doubles = {1.0,2.0,4.0};
