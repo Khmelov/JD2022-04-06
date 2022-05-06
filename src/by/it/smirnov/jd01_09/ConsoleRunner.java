e0d08cc3989a2e3c2c4038861c0e3039d315d7ad
Anton Smirnov <watashinodenwadesu@gmail.com>
2022-05-06 11:00:18 +0300

    jd01-09 tasks complete: amended by try-catch NullPointerException and start-ending wordings.

--- src/by/it/smirnov/jd01_09/ConsoleRunner.java
+++ src/by/it/smirnov/jd01_09/ConsoleRunner.java
@@ -3,6 +3,11 @@ package by.it.smirnov.jd01_09;
 import java.util.Scanner;
 
 public class ConsoleRunner {
+
+    static final String start = "Let's get started!\n" +
+            "Введите 2 операнда (скаляр, вектор или матрицу) и знак операции между ними (-+* или /).\n для выхода введите \"end\"";
+    static final String ending = "App work has successfully ended.\n You're welcome to try again. Have a nice time!";
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -10,9 +15,12 @@ public class ConsoleRunner {
         Parser parser = new Parser();
         Printer printer = new Printer();
 
+        System.out.println(start);
+
         while (scanner.hasNext()) {
             line = scanner.nextLine();
             if(line.equals("end")) {
+                System.out.println(ending);
                 break;
             }
 
