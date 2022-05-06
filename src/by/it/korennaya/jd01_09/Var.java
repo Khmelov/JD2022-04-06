414c5e8c5b54a7de3401139f42947effc99078b2
korennaya <kgv203@gmail.com>
2022-05-07 00:55:58 +0300

    Task JD01_08.oop finished

---
+++ src/by/it/korennaya/jd01_09/Var.java
@@ -0,0 +1,29 @@
+package by.it.korennaya.jd01_09;
+
+abstract class Var implements Operation {
+
+    @Override
+    public Var add(Var other) {
+        System.out.printf("Incorrect operation %s+%s%n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.printf("Incorrect operation %s-%s%n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.printf("Incorrect operation %s*%s%n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.printf("Incorrect operation %s/%s%n", this, other);
+        return null;
+    }
+
+}
