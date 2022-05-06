50acd25751b83ed9263043c11d50481d0ffb786b
Penkrat_Luda <ludmilapenkrat@gmail.com>
2022-05-06 23:04:52 +0300

    Task jd01_09 done completely

---
+++ src/by/it/penkrat/jd01_09/Printer.java
@@ -0,0 +1,12 @@
+package by.it.penkrat.jd01_09;
+
+import java.util.Objects;
+
+public class Printer {
+
+    public void print(Var result){
+        if(Objects.nonNull(result)){
+            System.out.println(result);
+        }
+    }
+}
