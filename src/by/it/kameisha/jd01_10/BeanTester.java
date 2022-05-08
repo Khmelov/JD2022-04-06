27103c766dd4cc993c8098b5cfd517f668dc009d
Kameisha <xim9052@gmail.com>
2022-05-09 00:23:10 +0300

    jd01_10 complete Task A,B         start    Task C1

--- src/by/it/kameisha/jd01_10/BeanTester.java
+++ src/by/it/kameisha/jd01_10/BeanTester.java
@@ -1,7 +1,21 @@
 package by.it.kameisha.jd01_10;
 
-public class BeanTester {
-    public static void main(String[] args) {
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+        Class<Bean> beanClass = Bean.class;
+        Method[] methods = beanClass.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Param.class)) {
+                Object object = beanClass.newInstance();
+                Param param = method.getAnnotation(Param.class);
+                System.out.println(param.a()+" "+param.b());
+                System.out.println(method.invoke(object,param.a(),param.b()));
+            }
+
+        }
     }
 }
+
