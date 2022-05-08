5c7aad7a85f63e624b1f2b8bb4bceb5b6e4b3f35
Kameisha <xim9052@gmail.com>
2022-05-08 11:15:26 +0300

    jd01_10 complete Task A

---
+++ src/by/it/kameisha/jd01_10/Bean.java
@@ -0,0 +1,22 @@
+package by.it.kameisha.jd01_10;
+
+public class Bean {
+    static double sum(int a, int b) {
+        return (double) (a + b);
+    }
+
+    @Param(a = 100, b = 50)
+    static double max(int a, int b) {
+        return (double) Math.max(a, b);
+    }
+
+    @Param(a = 10, b = 20)
+    double min(int a, int b) {
+        return (double) Math.min(a, b);
+    }
+
+    @Param(a = 30, b = 10)
+    double avg(int a, int b) {
+        return (a + b) / 2d;
+    }
+}
