5274faad10a413a90c7238240636de6f426b8a3b
Аврамчук Сергей <dvizenets@mail.ru>
2022-05-09 17:49:01 +0300

    jd01_10 level C complete

---
+++ src/by/it/avramchuk/jd01_10/BeanTester.java
@@ -0,0 +1,30 @@
+package by.it.avramchuk.jd01_10;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+
+        Class<Bean> beanClass = Bean.class;
+        Method[] allMethods = beanClass.getDeclaredMethods();
+        Constructor<?> constructor = beanClass.getConstructor();
+        Object o = constructor.newInstance();
+
+
+
+        for (Method method : allMethods) {
+            if (method.isAnnotationPresent(Param.class)){
+                if (Modifier.isStatic(method.getModifiers())){
+                    Param param = method.getAnnotation(Param.class);
+                    System.out.print(method.getName()+ " ");
+                    System.out.println(method.invoke(o, param.a(),param.b()));
+                }else {
+                    Param param = method.getAnnotation(Param.class);
+                    System.out.print(method.getName()+" ");
+                    System.out.println(method.invoke(o, param.a(), param.b()));
+                }
+            }
+        }
+    }
+}
