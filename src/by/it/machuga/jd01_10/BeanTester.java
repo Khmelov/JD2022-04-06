af36e58752e31b76a7caafa08289fe6b3fa075a8
Aleksandr Machuga <machuga.aleksandr@gmail.com>
2022-05-10 19:27:52 +0300

    updated 01-10 C

--- src/by/it/machuga/jd01_10/BeanTester.java
+++ src/by/it/machuga/jd01_10/BeanTester.java
@@ -2,11 +2,12 @@ package by.it.machuga.jd01_10;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class BeanTester {
     public static void main(String[] args) throws Exception {
         Class<Bean> beanClass = Bean.class;
-        Constructor<Bean> constructor = beanClass.getConstructor();
+        Constructor<Bean> constructor = beanClass.getDeclaredConstructor();
         Bean bean = constructor.newInstance();
         Method[] methods = beanClass.getDeclaredMethods();
         for (Method method : methods) {
@@ -14,8 +15,13 @@ public class BeanTester {
                 Param annotation = method.getAnnotation(Param.class);
                 int a = annotation.a();
                 int b = annotation.b();
-                Object result = method.invoke(bean, a, b);
-                System.out.println(method.getName() + " " + result);
+                Object result;
+                if (Modifier.isStatic(method.getModifiers())) {
+                    result = method.invoke(null, a, b);
+                } else {
+                    result = method.invoke(bean, a, b);
+                }
+                System.out.printf("%s -> %s%n", method.getName(), result);
             }
         }
     }
