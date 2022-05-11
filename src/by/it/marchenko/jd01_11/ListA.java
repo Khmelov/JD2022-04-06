807738250844d8441a6c97735428a831df217cc4
Delarez <92412420+Delarez@users.noreply.github.com>
2022-05-11 23:36:31 +0300

    jd01_11 start ListA created

---
+++ src/by/it/marchenko/jd01_11/ListA.java
@@ -0,0 +1,128 @@
+package by.it.marchenko.jd01_11;
+
+import java.util.*;
+
+
+public class ListA<E> implements List<E> {
+    private static int capacity = 0;
+    private static final int INITIAL_CAPACITY = 10;
+
+    private E[] elements = (E[]) new Object[INITIAL_CAPACITY];
+
+
+    @Override
+    public boolean add(E e) {
+    //    if (elements.length == capacity)
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return capacity;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
--- src/by/it/marchenko/jd01_11/Runner.java
+++ src/by/it/marchenko/jd01_11/Runner.java
@@ -9,15 +9,22 @@ public class Runner {
     public static final String SECOND_ELEMENT = "two";
     public static final String THIRD_ELEMENT = "three";
     public static final String FORTH_ELEMENT = "four";
-    public static final String FIFTH_ELEMENT = "fife";
+    public static final String FIFTH_ELEMENT = "five";
 
     public static void main(String[] args) {
-        List<String> stdList = new ArrayList<>(INITIAL_CAPACITY);
+        List<String> stdList = new ArrayList<>();
         stdList.add(FIRST_ELEMENT);
         stdList.add(SECOND_ELEMENT);
         stdList.add(THIRD_ELEMENT);
         stdList.add(FORTH_ELEMENT);
         stdList.add(FIFTH_ELEMENT);
         System.out.println(stdList);
+        stdList.remove(2);
+        System.out.println(stdList);
+        System.out.println(stdList.get(0));
+        System.out.println(stdList.get(stdList.size()-1));
+
+        List<String> myList = new ListA<>();
+
     }
 }
