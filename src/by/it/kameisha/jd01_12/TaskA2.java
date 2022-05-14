291e90720db8bb9b2ae484e973736fe5d43fea64
Kameisha <xim9052@gmail.com>
2022-05-14 22:13:40 +0300

    jd01_12 start task A3         complete task A1,A2

--- src/by/it/kameisha/jd01_12/TaskA2.java
+++ src/by/it/kameisha/jd01_12/TaskA2.java
@@ -1,4 +1,30 @@
 package by.it.kameisha.jd01_12;
+import java.util.*;
 
 public class TaskA2 {
+    public static void main(String[] args) {
+        List<Integer> one = Arrays.asList(3, -5, 5, 3, 1,-4,0,9,-1,5);
+        List<Integer> two = Arrays.asList(7,0,-6,1,2,-8,-2,3,3);
+        Set<Integer> first = new HashSet<>(one);
+        Set<Integer> second = new TreeSet<>(two);
+        System.out.println("First Set "+first);
+        System.out.println("Second Set "+second);
+        Set<Integer>cross = getCross(first,second);
+        System.out.println("Cross "+ cross);
+        Set<Integer>union = getUnion(first,second);
+        System.out.println("Union "+union);
+
+    }
+
+    private static Set<Integer> getCross(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> result = new HashSet<>(first);
+        result.retainAll(second);
+        return result;
+    }
+
+    private static Set<Integer> getUnion(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> result = new HashSet<>(first);
+        result.addAll(second);
+        return result;
+    }
 }
---
+++ src/by/it/kameisha/jd01_12/TaskA3.java
@@ -0,0 +1,9 @@
+package by.it.kameisha.jd01_12;
+
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
