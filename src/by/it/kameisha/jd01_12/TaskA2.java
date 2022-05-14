4a9bd5e063f0835976b765947bf329207456814e
Kameisha <xim9052@gmail.com>
2022-05-14 22:35:49 +0300

    jd01_12 start task B1         complete task A

--- src/by/it/kameisha/jd01_12/TaskA2.java
+++ src/by/it/kameisha/jd01_12/TaskA2.java
@@ -1,18 +1,19 @@
 package by.it.kameisha.jd01_12;
+
 import java.util.*;
 
 public class TaskA2 {
     public static void main(String[] args) {
-        List<Integer> one = Arrays.asList(3, -5, 5, 3, 1,-4,0,9,-1,5);
-        List<Integer> two = Arrays.asList(7,0,-6,1,2,-8,-2,3,3);
+        List<Integer> one = Arrays.asList(3, -5, 5, 3, 1, -4, 0, 9, -1, 5);
+        List<Integer> two = Arrays.asList(7, 0, -6, 1, 2, -8, -2, 3, 3);
         Set<Integer> first = new HashSet<>(one);
         Set<Integer> second = new TreeSet<>(two);
-        System.out.println("First Set "+first);
-        System.out.println("Second Set "+second);
-        Set<Integer>cross = getCross(first,second);
-        System.out.println("Cross "+ cross);
-        Set<Integer>union = getUnion(first,second);
-        System.out.println("Union "+union);
+        System.out.println("First Set " + first);
+        System.out.println("Second Set " + second);
+        Set<Integer> cross = getCross(first, second);
+        System.out.println("Cross " + cross);
+        Set<Integer> union = getUnion(first, second);
+        System.out.println("Union " + union);
 
     }
 
--- src/by/it/kameisha/jd01_12/TaskA3.java
+++ src/by/it/kameisha/jd01_12/TaskA3.java
@@ -8,17 +8,17 @@ public class TaskA3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Integer> input = new ArrayList<>();
-        int posZero =0;
-        while (scanner.hasNext()){
+        int posZero = 0;
+        while (scanner.hasNext()) {
             String value = scanner.next();
-            if(!value.equals("end")){
+            if (!value.equals("end")) {
                 int i = Integer.parseInt(value);
-                if(i<0){
+                if (i < 0) {
                     input.add(i);
-                } else if (i==0) {
-                    input.add(posZero,i);
+                } else if (i == 0) {
+                    input.add(posZero, i);
                 } else {
-                    input.add(posZero++,i);
+                    input.add(posZero++, i);
                 }
             }
         }
