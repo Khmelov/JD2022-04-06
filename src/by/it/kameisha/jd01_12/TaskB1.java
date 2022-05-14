5b7dacd768c357929fcec917aeb629b79ad2ccbb
Kameisha <xim9052@gmail.com>
2022-05-14 22:54:01 +0300

    jd01_12 start task B1         complete task A

--- src/by/it/kameisha/jd01_12/TaskB1.java
+++ src/by/it/kameisha/jd01_12/TaskB1.java
@@ -1,4 +1,16 @@
 package by.it.kameisha.jd01_12;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Pattern pattern = Pattern.compile("[a-zA-Z']+");
+
+        Matcher matcher = pattern.matcher(scanner.next(pattern));
+
+
+    }
 }
