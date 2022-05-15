65ee10954cf59013c03fed4fdec1d4c5f7cd4b5c
Delarez <92412420+Delarez@users.noreply.github.com>
2022-05-15 21:59:01 +0300

    jd01_12 Task B2 complete

---
+++ src/by/it/marchenko/jd01_12/TaskB2.java
@@ -0,0 +1,57 @@
+package by.it.marchenko.jd01_12;
+
+import java.util.*;
+
+public class TaskB2 {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String[] peoples = getInputData(console);
+
+        ArrayList<String> peoplesArrayList = new ArrayList<>(Arrays.asList(peoples));
+        LinkedList<String> peoplesLinkedList = new LinkedList<>(Arrays.asList(peoples));
+
+        System.out.printf("ArrayList winner  : %s", process(peoplesArrayList));
+        System.out.printf("LinkedList winner : %s", process(peoplesLinkedList));
+    }
+
+    private static String[] getInputData(Scanner input) {
+        System.out.print("Enter peoples amount: ");
+        int peopleAmount = input.nextInt();
+        String[] peopleNames = new String[peopleAmount];
+        for (int i = 0; i < peopleNames.length; i++) {
+            peopleNames[i] = input.next();
+        }
+        return peopleNames;
+    }
+
+    static String process(ArrayList<String> peoples) {
+        boolean toRemove = true;
+        while (peoples.size() != 1) {
+            Iterator<String> iterator = peoples.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                toRemove = !toRemove;
+                if (toRemove) {
+                    iterator.remove();
+                }
+            }
+        }
+        return peoples.get(0);
+    }
+
+    static String process(LinkedList<String> peoples) {
+        boolean toRemove = true;
+        while (peoples.size() != 1) {
+            Iterator<String> iterator = peoples.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                toRemove = !toRemove;
+                if (toRemove) {
+                    iterator.remove();
+                }
+            }
+        }
+        return peoples.get(0);
+    }
+}
