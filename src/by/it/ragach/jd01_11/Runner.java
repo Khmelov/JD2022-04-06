d440cfd176d1b5451a277804fa7ae1ba3857feec
Oksana Ragach <oksanaragach@gmail.com>
2022-05-15 18:41:14 +0300

    jd01-11 Task С start

--- src/by/it/ragach/jd01_11/Runner.java
+++ src/by/it/ragach/jd01_11/Runner.java
@@ -34,21 +34,6 @@ public class Runner {
 
 
 
-
-        list = new ListB<>();
-        list.add("one");
-        list.add("two");
-        list.add("three");
-        list.add("four");
-        list.add("five");
-        System.out.println(list);
-        list.remove(2);
-        System.out.println(list);
-        System.out.printf("first = %s last=%s%n",list.get(0), list.get(list.size()-1));
-        System.out.println(list.set(3,"seven"));
-        System.out.println(list);
-
-
         HashSet<String> set = new HashSet<>();
         set.add("six");
         set.add("seven");
@@ -58,13 +43,7 @@ public class Runner {
         System.out.println(set);
 
 
-        SetC<String>setC =new SetC<>();
-        set.add("six");
-        set.add("seven");
-        set.add("eight");
-        set.add("nine");
-        set.add("ten");
-        System.out.println(setC);
+
 
 
     }
