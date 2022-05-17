5d077fb2bc317eafce772fea60b739dc7a4ceff3
Oksana Ragach <oksanaragach@gmail.com>
2022-05-17 18:04:53 +0300

    jd01-12 Task B1 in process

--- src/by/it/ragach/jd01_12/TaskB1.java
+++ src/by/it/ragach/jd01_12/TaskB1.java
@@ -6,35 +6,42 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+    public class TaskB1 {
+        public static void main(String[] args) {
 
-public class TaskB1 {
-    public static void main(String[] args) {
-        String values = "crocodile cat cat cat cat dog dog don't cat frog mouse pig Horse horse elephant end";
-        Scanner scanner = new Scanner(values);
-        Map<String,Integer> wordToCount = new HashMap<>();
-        for(String word: values.split(" ")) {
-            if (!word.equals("end")) {
-                if (wordToCount.containsKey(word)) {
-                    wordToCount.put(word, wordToCount.get(word) + 1);
-                } else {
-                    wordToCount.put(word, 1);
+            Scanner scanner = new Scanner(System.in);
+
+
+            Map<String, Integer> wordToCount = new HashMap<>();
+            while (scanner.hasNext()) {
+                String value = scanner.nextLine();
+
+                for (String word : value.split("[^a-zA-Z]+")) {
+                    if (!word.equals("end")) {
+                        if (wordToCount.containsKey(word)) {
+                            wordToCount.put(word, wordToCount.get(word) + 1);
+                        } else {
+                            wordToCount.put(word, 1);
+                        }
+                    } else {
+                        break;
+                    }
                 }
-            } else {
-                break;
+
+                Set<Map.Entry<String, Integer>> setWord = wordToCount.entrySet();
+                Iterator<Map.Entry<String, Integer>> iWord = setWord.iterator();
+                while (iWord.hasNext()) {
+                    Map.Entry<String, Integer> me = iWord.next();
+                    System.out.println(me.getKey() + "=" + me.getValue());
+
+                }
             }
         }
-
-        Set<Map.Entry<String,Integer>> setWord = wordToCount.entrySet();
-        Iterator<Map.Entry<String,Integer>>iWord = setWord.iterator();
-        while (iWord.hasNext()){
-            Map.Entry<String,Integer>me =iWord.next();
-            System.out.println(me.getKey()+"="+me.getValue());
-
-        }
-        System.out.println(values);
-
     }
-}
+
+
+
+
 
 
 
