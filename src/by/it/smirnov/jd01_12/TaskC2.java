b375fb77bcdf12a41a4cdf7728fb89242b485b30
Anton Smirnov <watashinodenwadesu@gmail.com>
2022-05-18 09:41:35 +0300

    jd01-12 TaskC2 not complete.

--- src/by/it/smirnov/jd01_12/TaskC2.java
+++ src/by/it/smirnov/jd01_12/TaskC2.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class TaskC2 {
 
-    private static Set<Long> a = new HashSet<>() ;
+    private static Set<Long> a = new HashSet<>();
 
     private static Set<Byte> b = new TreeSet<>();
 
@@ -15,39 +15,45 @@ public class TaskC2 {
         a.addAll(Arrays.asList(1L, 2L, 3L, 5L, 7L, 8L, 34L, 67L, 8L, 2L));
         Byte[] array = {11, 12, 13, 14, 17, 8, 34, 67, 8, 99, 101};
         b.addAll(Arrays.asList(array));
-        c.addAll(Arrays.asList(11.0F, 12.0F, 13.0F, 14.0F, 17.0F, 8.0F, 34.0F, 67.0F, 8.0F, 99.0F, 101.0F));
-        System.out.println(a.toString());
-        System.out.println(b.toString());
-        System.out.println(c.toString());
+        c.addAll(Arrays.asList(11.0F, 12.0F, 13.0F, 14.0F, 17.0F, 8.0F, 34.0F));
         Set<Number> crossed = getCross(a, b, c);
         Set<Number> united = getUnion(a, b, c);
         System.out.println(crossed);
         System.out.println(united);
-
     }
 
     private static HashSet<Number> getCross(Set<Long> a, Set<Byte> b, Set<Float> c) {
         HashSet<Number> crossed = new HashSet<>();
-
-
+        for (Byte aByte : b) {
+            for (Long aLong : a) {
+                for (Float aFloat : c) {
+                    if (compadre(aByte, aLong) && compadre(aByte, aFloat)) crossed.add(aByte);
+                }
+            }
+        }
         return crossed;
     }
 
-    private static int compadre (Number a, Number b) {
+    private static boolean compadre(Number a, Number b) {
         BigDecimal a1 = new BigDecimal(a.toString());
         BigDecimal b1 = new BigDecimal(b.toString());
-        return (a1.compareTo(b1));
+        return (a1.compareTo(b1)) == 0;
     }
 
     private static HashSet<Number> getUnion(Set<Long> a, Set<Byte> b, Set<Float> c) {
         HashSet<Number> united = new HashSet<>(a);
-        united.addAll(b);
+        for (Long aLong : a) {
+            for (Byte aByte : b) {
+                for (Float aFloat : c) {
+                    if (compadre(aByte, aLong) && compadre(aByte, aFloat)) continue;
+                    else if (compadre(aByte, aFloat)) united.add(aLong);
+                    else {
+                        united.add(aLong);
+                        united.add(aFloat);
+                    }
+                }
+            }
+        }
         return united;
     }
-
-
 }
-
-
-    //private static void iterator() {
-    //}
