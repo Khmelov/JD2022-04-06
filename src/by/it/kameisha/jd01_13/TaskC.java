6b290dec84a65d3e04fa8179d1c37232c6a4a517
Kameisha <xim9052@gmail.com>
2022-05-19 21:40:04 +0300

    jd01_13 complete A,B,C

--- src/by/it/kameisha/jd01_13/TaskC.java
+++ src/by/it/kameisha/jd01_13/TaskC.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class TaskC {
     static LinkedList<String> consoleStrings = new LinkedList<>();
+    static LinkedList<Double> numbers = new LinkedList<>();
     static int count = 0;
 
     public static void main(String[] args) {
@@ -19,18 +20,19 @@ public class TaskC {
             if (consoleStrings.size() != 0) {
                 String string = consoleStrings.getLast();
                 Double number = Double.parseDouble(string);
+                numbers.add(number);
             }
         } catch (NumberFormatException e) {
             pause();
             count++;
             consoleStrings.removeLast();
             StringJoiner reversInput = new StringJoiner(" ");
-            for (int i = consoleStrings.size() - 1; i >= 0; i--) {
-                reversInput.add(consoleStrings.get(i));
+            for (int i = numbers.size() - 1; i >= 0; i--) {
+                reversInput.add(numbers.get(i).toString());
             }
             System.out.println(reversInput);
         }
-        if (count == 6) {
+        if (count == 5) {
             throw new NumberFormatException();
         }
     }
