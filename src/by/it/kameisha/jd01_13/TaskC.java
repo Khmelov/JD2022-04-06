208a41cc9cf909f981e02e514f79fcf11bb2ef4c
Kameisha <xim9052@gmail.com>
2022-05-19 20:35:56 +0300

    jd01_13 start task C         complete A,B

--- src/by/it/kameisha/jd01_13/TaskC.java
+++ src/by/it/kameisha/jd01_13/TaskC.java
@@ -1,7 +1,45 @@
 package by.it.kameisha.jd01_13;
 
+import java.util.*;
+
 public class TaskC {
+    static LinkedList<String> consoleStrings = new LinkedList<>();
+    static int count = 0;
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            consoleStrings.add(scanner.next());
+            readData();
+        }
+    }
+
+    static void readData() throws NumberFormatException {
+        try {
+            if (consoleStrings.size() != 0) {
+                String string = consoleStrings.getLast();
+                Double number = Double.parseDouble(string);
+            }
+        } catch (NumberFormatException e) {
+            pause();
+            count++;
+            consoleStrings.removeLast();
+            StringJoiner reversInput = new StringJoiner(" ");
+            for (int i = consoleStrings.size() - 1; i >= 0; i--) {
+                reversInput.add(consoleStrings.get(i));
+            }
+            System.out.println(reversInput);
+        }
+        if (count == 6) {
+            throw new NumberFormatException();
+        }
+    }
+
+    private static void pause() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
