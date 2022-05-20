f2d8b370d692a1c607784f10a882dd7018e87d32
kudelkoartem <sinergia86@yandex.ru>
2022-05-20 15:44:13 +0300

    jd01-13 A+B finished. Kudelko.

---
+++ src/by/it/kudelko/jd01_13/TaskB.java
@@ -0,0 +1,53 @@
+package by.it.kudelko.jd01_13;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<>();
+        try {
+            while (sc.hasNext()) {
+                String value = sc.next();
+                if (!value.equals("END")) {
+                    double element = Double.parseDouble(value);
+                    list.add(element);
+                } else {
+                    break;
+                }
+            }
+            double b = 0;
+            double z = 0;
+            for (int j = 0; j < list.size(); j++) {
+                b = b + list.get(j);
+                z = Math.sqrt(b);
+                if (Double.isNaN(z)) {
+                    throw new ArithmeticException();
+                }
+                System.out.println(list.get(j));
+                System.out.println(z);
+            }
+
+
+        } catch (NumberFormatException | NullPointerException | ArithmeticException e) {
+            showDetails(e);
+        }
+    }
+
+
+    static void showDetails(RuntimeException e) {
+        Class<? extends RuntimeException> exceptionClass = e.getClass();
+        Class<TaskB> currentClass = TaskB.class;
+        String exceptionName = exceptionClass.getName();
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        for (StackTraceElement element : stackTrace) {
+            String className = element.getClassName();
+            if (className.equals(currentClass.getName())) {
+                int numberElement = element.getLineNumber();
+                System.out.printf(" name: %s%nclass: %s%n line: %d", exceptionName, className, numberElement);
+                break;
+            }
+        }
+    }
+}
