18966c918310f662de575abe5362ed20e9612050
Kameisha <xim9052@gmail.com>
2022-05-20 20:39:01 +0300

    jd01_13 fix calculator         complete A,B,C

--- src/by/it/kameisha/calc/Var.java
+++ src/by/it/kameisha/calc/Var.java
@@ -9,26 +9,24 @@ abstract class Var implements Operation {
     }
 
     @Override
-    public Var add(Var other) {
-        System.out.printf("Incorrect operation %s + %s%n", this, other);
-        return null;
+    public Var add(Var other) throws CalcException {
+        throw new CalcException("Incorrect operation %s + %s%n", this, other);
     }
 
     @Override
-    public Var sub(Var other) {
-        System.out.printf("Incorrect operation %s - %s%n", this, other);
-        return null;
+    public Var sub(Var other) throws CalcException {
+        throw new CalcException("Incorrect operation %s - %s%n", this, other);
+
     }
 
     @Override
-    public Var mul(Var other) {
-        System.out.printf("Incorrect operation %s * %s%n", this, other);
-        return null;
+    public Var mul(Var other) throws CalcException {
+        throw new CalcException("Incorrect operation %s * %s%n", this, other);
+
     }
 
     @Override
-    public Var div(Var other) {
-        System.out.printf("Incorrect operation %s / %s%n", this, other);
-        return null;
+    public Var div(Var other) throws CalcException {
+        throw new CalcException("Incorrect operation %s / %s%n", this, other);
     }
 }
