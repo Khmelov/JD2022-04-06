f7cc55146354c7d023b4c5cd2e5b30d610a14f0e
Oksana Ragach <oksanaragach@gmail.com>
2022-05-20 23:26:33 +0300

    jd01-13 Task B changed

--- src/by/it/ragach/jd01_13/TaskB.java
+++ src/by/it/ragach/jd01_13/TaskB.java
@@ -1,33 +1,43 @@
 package by.it.ragach.jd01_13;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TaskB {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        double sum = 0;
 
 
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
-            if (!line.equals("end")) {
+            if (line.equals("end")) {
+                break;}
+            else {
                 try {
-                    String[] sentence = line.split("\n");
-                    double result = 0;
-                    int sum = 0;
-                    for (int i = 0; i < sentence.length; i++) {
-                        sentence[i] = String.valueOf(Integer.parseInt(sentence[i]));
-                        sum = Integer.parseInt(sum+sentence[i]);
-                        result = Math.sqrt(sum);
-                        System.out.println(sentence);
-
+                    double start = Double.parseDouble(line);
+                    sum = sum + start;
+                    double result = Math.sqrt(sum);
+                    
+                } catch (NumberFormatException | ArithmeticException e) {
+                    Class<? extends RuntimeException> exceptionClass = e.getClass();
+                    Class<TaskB> currentClass = TaskB.class;
+                    String exceptionName = exceptionClass.getName();
+                    StackTraceElement[] strackTrace = e.getStackTrace();
+                    for (StackTraceElement element : strackTrace) {
+                        String classname = element.getClassName();
+                        if (classname.equals(currentClass.getName())) {
+                            int lineNumber = element.getLineNumber();
+                            System.out.printf("""
+                                    name: %s
+                                    class: %s
+                                    line: %d
+                                    """, exceptionName, classname, lineNumber);
+                        }
                     }
-
-                } catch (NumberFormatException|ArithmeticException e) {
-                    e.printStackTrace();
                 }
-            }else {
-                break;
+
             }
         }
 
