c268765cee5417aea5f78720d2bf057eb7848334
Kameisha <xim9052@gmail.com>
2022-05-22 00:32:54 +0300

    jd01_14 start task B         complete task A

--- src/by/it/kameisha/jd01_14/TaskB.java
+++ src/by/it/kameisha/jd01_14/TaskB.java
@@ -1,4 +1,20 @@
 package by.it.kameisha.jd01_14;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TaskB {
+    public static final String POEM_TEXT = "Poem.text";
+
+    public static void main(String[] args) {
+        String path = Util.getPath(TaskB.class, POEM_TEXT);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            while(bufferedReader.read()!=-1){
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
