fcf00706300f366adf81d628ad3beb2baa8753a6
Oksana Ragach <oksanaragach@gmail.com>
2022-05-21 22:59:10 +0300

    jd01-13 Task B completed

--- src/by/it/ragach/jd01_13/TaskB.java
+++ src/by/it/ragach/jd01_13/TaskB.java
@@ -1,6 +1,5 @@
 package by.it.ragach.jd01_13;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class TaskB {
@@ -9,40 +8,50 @@ public class TaskB {
         Scanner scanner = new Scanner(System.in);
         double sum = 0;
 
-
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            if (line.equals("end")) {
-                break;}
-            else {
-                try {
+        try {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.equals("END")) {
+                    break;
+                } else {
                     double start = Double.parseDouble(line);
                     sum = sum + start;
                     double result = Math.sqrt(sum);
-                    
-                } catch (NumberFormatException | ArithmeticException e) {
-                    Class<? extends RuntimeException> exceptionClass = e.getClass();
-                    Class<TaskB> currentClass = TaskB.class;
-                    String exceptionName = exceptionClass.getName();
-                    StackTraceElement[] strackTrace = e.getStackTrace();
-                    for (StackTraceElement element : strackTrace) {
-                        String classname = element.getClassName();
-                        if (classname.equals(currentClass.getName())) {
-                            int lineNumber = element.getLineNumber();
-                            System.out.printf("""
-                                    name: %s
-                                    class: %s
-                                    line: %d
-                                    """, exceptionName, classname, lineNumber);
-                        }
+                    System.out.println(start);
+                    System.out.println(result);
+                    if (Double.isNaN(result)) {
+                        throw new ArithmeticException();
                     }
-                }
 
+                }
             }
+
+        } catch (ArithmeticException | NumberFormatException e) {
+            Class<TaskB> currentClass = TaskB.class;
+            Class<? extends RuntimeException> exceptionClass = e.getClass();
+            String exceptionName = exceptionClass.getName();
+            StackTraceElement[] strackTrace = e.getStackTrace();
+            for (StackTraceElement element : strackTrace) {
+                String classname = element.getClassName();
+                if (classname.equals(currentClass.getName())) {
+                    int lineNumber = element.getLineNumber();
+                    System.out.printf("""
+                            name: %s
+                            class: %s
+                            line: %d
+                            """, exceptionName, classname, lineNumber);
+                }
+            }
+
         }
 
     }
+
 }
+
+
+
+
 
 
 
