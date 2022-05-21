17d391136bf37584cd2c83386f8475056107e28e
Kameisha <xim9052@gmail.com>
2022-05-21 20:26:08 +0300

    jd01_14 start task A

--- src/by/it/kameisha/jd01_14/TaskA.java
+++ src/by/it/kameisha/jd01_14/TaskA.java
@@ -1,4 +1,11 @@
 package by.it.kameisha.jd01_14;
 
 public class TaskA {
+    public static final String USER_DIR = "user.dir";
+    public static final String DATA_TASK_A_BIN = "dataTaskA.bin";
+    public static final String RESULT_TASK_A_TXT = "resultTaskA.txt";
+
+    public static void main(String[] args) {
+        
+    }
 }
