e557b65251e15f934ee993219e75fb16d05ca13e
Kameisha <xim9052@gmail.com>
2022-05-22 23:13:04 +0300

    jd01_14 fix calculator         complete tasks A,B,C

--- src/by/it/kameisha/jd01_14/TaskC.java
+++ src/by/it/kameisha/jd01_14/TaskC.java
@@ -1,24 +1,47 @@
 package by.it.kameisha.jd01_14;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class TaskC {
+
+    public static final String RESULT_TASK_C_TXT = "resultTaskC.txt";
+
     public static void main(String[] args) {
-        String root = System.getProperty(TaskA.USER_DIR);
-        String name = Util.getPath(TaskC.class,"");
-        String newname = name.replace("jd01_14\\","");
-        String path = "D:\\itacademy\\JD2022-04-06"+File.separator+"by"+File.separator+"it"+File.separator+"kameisha";
-        File file = new File(newname);
-        System.out.println(newname);
-        System.out.println(file.isDirectory());
-        File[] list = file.listFiles();
-        for (File file1 : list) {
-            if(file1.isDirectory()){
-                System.out.println(" dir:"+file1.getName());
-                for (File listFile : file1.listFiles()) {
-                    System.out.println("file:"+listFile.getName());
+        Class<TaskC> taskCClass = TaskC.class;
+        String directoryPath = Util.getPath(taskCClass, "").replace("jd01_14\\", "");
+        String resultPath = Util.getPath(taskCClass, RESULT_TASK_C_TXT);
+        File directory = new File(directoryPath);
+        printDirectory(directory);
+        printTxtFile(resultPath, directory);
+    }
+
+    private static void printDirectory(File directory) {
+        File[] files = directory.listFiles();
+        for (File file : files != null ? files : new File[0]) {
+            if (!file.isDirectory()) {
+                System.out.println("file:" + file.getName());
+            } else {
+                System.out.println(" dir:" + file.getName());
+                printDirectory(file);
+            }
+        }
+    }
+
+    private static void printTxtFile(String resultPath, File directory) {
+        try (PrintWriter printWriter = new PrintWriter(resultPath)) {
+            File[] files = directory.listFiles();
+            for (File file : files != null ? files : new File[0]) {
+                if (!file.isDirectory()) {
+                    printWriter.println("file:" + file.getName());
+                } else {
+                    printWriter.println(" dir:" + file.getName());
+                    printDirectory(file);
                 }
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
