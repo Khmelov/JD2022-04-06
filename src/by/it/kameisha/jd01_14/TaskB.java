b74d57e9a6d917740168cf088910e285f13f77a4
Kameisha <xim9052@gmail.com>
2022-05-22 15:10:57 +0300

    jd01_14 start task B         complete task A

--- src/by/it/kameisha/jd01_14/TaskB.java
+++ src/by/it/kameisha/jd01_14/TaskB.java
@@ -1,18 +1,41 @@
 package by.it.kameisha.jd01_14;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskB {
-    public static final String POEM_TEXT = "Poem.text";
+    public static final String POEM_TEXT = "Poem.txt";
+    public static final String RESULT_TASK_B_TXT = "resultTaskB.txt";
+    public static final String RUSSIAN_WORDS = "[а-яА-ЯёЁ]+";
+    public static final String PUNCTUATION_MARKS = "[,.:;!?]";
 
     public static void main(String[] args) {
-        String path = Util.getPath(TaskB.class, POEM_TEXT);
+        String pathPoemText = Util.getPath(TaskB.class, POEM_TEXT);
+        String pathResultTaskBText = Util.getPath(TaskB.class, RESULT_TASK_B_TXT);
+        printWordsAndPunctuationMarksCounts(pathPoemText);
+    }
+
+    private static void printWordsAndPunctuationMarksCounts(String path) {
+        Pattern patternWords = Pattern.compile(RUSSIAN_WORDS);
+        Pattern patternPunctuationMarks = Pattern.compile(PUNCTUATION_MARKS);
+        int countWords = 0;
+        int countPunctuationMarks = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
-            while(bufferedReader.read()!=-1){
+            String line;
+            while (null != (line = bufferedReader.readLine())) {
+                Matcher matcherWords = patternWords.matcher(line);
+                Matcher matcherPunctuationMarks = patternPunctuationMarks.matcher(line);
+                while (matcherWords.find()) {
+                    countWords++;
+                }
+                while (matcherPunctuationMarks.find()) {
+                    countPunctuationMarks++;
+                }
             }
+            System.out.println("words=" + countWords + ", punctuation marks=" + countPunctuationMarks);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
