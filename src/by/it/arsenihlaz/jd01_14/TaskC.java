440a503eb477c93ed57abfd13ae4ccec97e19e0c
Arseni Hlaz <mafusaik@gmail.com>
2022-05-23 20:01:29 +0300

    jd01_14 A, B, C done

--- src/by/it/arsenihlaz/jd01_14/TaskC.java
+++ src/by/it/arsenihlaz/jd01_14/TaskC.java
@@ -1,29 +1,69 @@
 package by.it.arsenihlaz.jd01_14;
 
 import by.it._classwork_.jd01_14.TaskA;
+import by.it._classwork_.jd01_14.Util;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.List;
 
 public class TaskC {
     public static void main(String[] args) {
         System.out.println(packageWay());
+
+
         File packages = new File(packageWay());
-        System.out.println(Arrays.toString(packages.list()));
-       // System.out.println(Arrays.toString(packages.listFiles()));
-
-        List<File> list = new ArrayList<>();
-        for (File file : packages.listFiles()) {
-            if (file. isDirectory()){
-                System.out.println(Arrays.toString(file.list()));
+        for (String pack : packages.list()) {
+            System.out.println("dir:" + pack);
+        }
+        for (File directory : packages.listFiles()) {
+            if (directory.isDirectory()) {
+                for (String file : directory.list()) {
+                    if (file.contains(".")) {
+                        System.out.println("file:" + file);
+                    } else {
+                        System.out.println("dir:" + file);
+                    }
+                }
             }
-
+            for (File subDirectory : directory.listFiles()) {
+                if (subDirectory.isDirectory()) {
+                for (String file : subDirectory.list()) {
+                    if (file.contains(".")) {
+                        System.out.println("file:" + file);
+                    } else {
+                        System.out.println("dir:" + file);
+                    }
+                }
+            }
+            }
         }
 
-        System.out.println(list);
+
+        String txtFile = Util.getPath(by.it.arsenihlaz.jd01_14.TaskA.class, "resultTaskC.txt");
+        try (PrintWriter out = new PrintWriter(txtFile)) {
+            out.println(packageWay());
+            File packages2 = new File(packageWay());
+            for (String pack : packages2.list()) {
+                out.println("dir:" + pack);
+            }
+            for (File files : packages2.listFiles()) {
+                if (files.isDirectory()) {
+                    for (String file : files.list()) {
+                        if (file.contains(".")) {
+                            out.println("file:" + file);
+                        } else {
+                            out.println("dir:" + file);
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     private static String packageWay() {
         String root = System.getProperty(TaskA.USER_DIR);
@@ -34,4 +74,6 @@ public class TaskC {
         String parent = path.getParent();
         return root + File.separator + "src" + File.separator + parent;
     }
+
+
 }
