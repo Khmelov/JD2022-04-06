ddc92114b17caa5dd3ddefe3e7073c269de69690
Oksana Ragach <oksanaragach@gmail.com>
2022-05-23 22:48:53 +0300

    jd01-14 Task B start

---
+++ src/by/it/ragach/jd01_14/TaskB.java
@@ -0,0 +1,30 @@
+package by.it.ragach.jd01_14;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+
+        String pathToBinaryFile = Util.getPath(TaskA.class,"Poem.txt");
+
+        try (DataInputStream dataInputStream = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream("Poem.txt"))
+                    )
+            ){
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        double count = 0;
+
+
+
+
+        }
+
+    }
+}
