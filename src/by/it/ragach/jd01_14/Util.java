146d11e46504231a00e2583727c1ba7dd3eebe8c
Оксана Рагач <oksanaragach@gmail.com>
2022-05-24 17:18:40 +0300

    jd01-14 Task B completed

--- src/by/it/ragach/jd01_14/Util.java
+++ src/by/it/ragach/jd01_14/Util.java
@@ -6,7 +6,7 @@ public class Util {
     public static String getPath(Class<?> aClass, String filename) {
         String root = System.getProperty(TaskA.USER_DIR);
         String relationPath =aClass.getName()
-                .replace(TaskA.class.getSimpleName(), "")
+                .replace(aClass.getSimpleName(), "")
                 .replace(".", File.separator);
         return root + File.separator + "src" + File.separator + relationPath+filename;
     }
