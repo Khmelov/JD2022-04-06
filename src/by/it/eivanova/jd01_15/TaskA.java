e62b25e6a1ef163fc1f519ebb6f313e470edd32c
Alena Ivanova <alena.ivanova.sk@gmail.com>
2022-05-25 21:42:27 +0300

    jd01_15 start C Ivanova

---
+++ src/by/it/eivanova/jd01_15/TaskA.java
@@ -0,0 +1,19 @@
+package by.it.eivanova.jd01_15;
+
+public class TaskA {
+
+    private static final int ROWS = 6;
+    private static final int COLS = 4;
+    private static final int MIN_VALUE = -15;
+    private static final int MAX_VALUE = 15;
+
+
+
+    public static void main(String[] args) {
+        int matrix = createMatrix(ROWS, COLS, MIN_VALUE, MAX_VALUE);
+    }
+
+    private static int createMatrix(int rows, int cols, int minValue, int maxValue) {
+        return 0;
+    }
+}
