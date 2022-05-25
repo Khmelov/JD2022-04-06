3c1d49d6bfbc19042c9dc73530d061b5b01dafe7
Arseni Hlaz <mafusaik@gmail.com>
2022-05-25 22:50:43 +0300

    Calculator fix

--- src/by/it/arsenihlaz/Calculator/Var.java
+++ src/by/it/arsenihlaz/Calculator/Var.java
@@ -6,30 +6,7 @@ import java.util.Objects;
 
 abstract class Var implements Operation {
     private Object var;
-    private static final Map<String, Var> vars = new HashMap<>();
 
-    public static Var createVar(String stringVar) throws CalcException {
-        Var result;
-        if (stringVar.matches(Patterns.SCALAR)) {
-            result = new Scalar(stringVar);
-        } else if (stringVar.matches(Patterns.VECTOR)) {
-            result = new Vector(stringVar);
-        } else if (stringVar.matches(Patterns.MATRIX)) {
-            result = new Matrix(stringVar);
-        } else {
-            result = vars.get(stringVar);
-        }
-        if (Objects.isNull(result)) {
-            throw new CalcException("Incorrect string %s", stringVar);
-        }
-        return result;
-    }
-
-    public static Var save(String name, Var value) {
-        vars.put(name, value);
-
-        return value;
-    }
 
     @Override
     public Var add(Var other) throws CalcException {
---
+++ src/by/it/arsenihlaz/jd01_15/TaskA.java
@@ -0,0 +1,7 @@
+package by.it.arsenihlaz.jd01_15;
+
+public class TaskA {
+    public static void main(String[] args) {
+
+    }
+}
