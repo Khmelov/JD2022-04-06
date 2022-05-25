db9c50a10cc9e0935a9747a46b19b9e66c06575c
kudelkoartem <sinergia86@yandex.ru>
2022-05-25 16:00:40 +0300

    jd01-14 A finished. Kudelko.

--- src/by/it/kudelko/jd01_14/TaskA.java
+++ src/by/it/kudelko/jd01_14/TaskA.java
@@ -1,23 +1,71 @@
 package by.it.kudelko.jd01_14;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TaskA {
 
     public static final String USER_DIR = "user.dir";
 
-    public static void main(String[] args) {
-        String path = getPath(TaskA.class, "dataTaskA.bin");
-
+    public TaskA() {
     }
 
-    public static String getPath(Class<?> aClass, String filename) {
-        String root = System.getProperty(USER_DIR);
-        System.out.println(root);
-        Class<TaskA> taskAClass = TaskA.class;
-        String name = taskAClass.getName();
-        String relationPath = name.replace(taskAClass.getSimpleName(), "").replace(".", File.separator);
-        String fullPath = root + File.separator + "src" + File.separator + relationPath;
-        System.out.println(fullPath);
+    public static void main(String[] args) {
+        String path = Util.getPath(TaskA.class, "dataTaskA.bin");
+        try (
+                DataOutputStream dataOutputStream = new DataOutputStream(
+                        new BufferedOutputStream(
+                                new FileOutputStream(path)
+                        )
+                )
+        ) {
+            for (int i = 0; i < 20; i++) {
+                int value = ThreadLocalRandom.current().nextInt(0, 1000);
+                dataOutputStream.writeInt(value);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<Integer> integers = new ArrayList<>();
+        try (
+                DataInputStream dataInputStream = new DataInputStream(
+                        new BufferedInputStream(
+                                new FileInputStream(path)
+                        )
+                )
+        ) {
+            while (dataInputStream.available() > 0) {
+                integers.add(dataInputStream.readInt());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        double sum = 0;
+        double count = 0;
+        for (Integer integer : integers) {
+            sum = sum + integer;
+            count++;
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+        System.out.println("avg=" + sum / count);
+
+        String txtFile = Util.getPath(TaskA.class, "resultTaskA.txt");
+        try (PrintWriter out = new PrintWriter(txtFile)) {
+            double sum2 = 0;
+            double count2 = 0;
+            for (Integer integer : integers) {
+                sum2 = sum2 + integer;
+                count2++;
+                out.print(integer + " ");
+            }
+            System.out.println();
+            System.out.println("avg=" + sum2 / count2);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
