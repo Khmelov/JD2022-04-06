31e70ba443195a8d4f9a0afa69301a5b3cbf2f70
Kameisha <xim9052@gmail.com>
2022-05-27 19:03:58 +0300

    jd01_15 start task B         complete A

--- src/by/it/kameisha/jd01_15/TaskB.java
+++ src/by/it/kameisha/jd01_15/TaskB.java
@@ -6,16 +6,33 @@ import java.io.*;
 
 public class TaskB {
     public static void main(String[] args) {
-        String pathClass = Util.getPath(TaskA.class, TaskA.class.getSimpleName() + ".java");
-        String pathTxtFile = Util.getPath(TaskA.class, TaskA.class.getSimpleName() + ".txt");
-        printClass(pathClass,pathTxtFile);
+        String pathClass = Util.getPath(TaskA.class, TaskB.class.getSimpleName() + ".java");
+        String pathTxtFile = Util.getPath(TaskA.class, TaskB.class.getSimpleName() + ".txt");
+        printClass(pathClass, pathTxtFile);
+        //comment 1
+        /*
+        hnrnrnrnrnrnr
+         */
+        System.out.println((byte) '*');
     }
 
     private static void printClass(String pathClass, String pathTxtFile) {
-        try(BufferedReader reader = new BufferedReader(new FileReader(pathClass));
-            PrintWriter writer = new PrintWriter(pathTxtFile)){
-            while (reader.readLine()!=null){
-                
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathClass));
+             PrintWriter writer = new PrintWriter(pathTxtFile)) {
+            //comment 2
+            while (reader.ready()) {
+                int first = reader.read();
+                if (first != 47) {
+                    writer.print((char) first);
+                } else {
+                    int second = reader.read();
+                    if (second == 47) {
+                        reader.readLine();
+                    } else if (second == 42) {
+
+                    }
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
