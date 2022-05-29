54ea825bb4d0ba5fbf31b471bf3a1c347597437f
Kameisha <xim9052@gmail.com>
2022-05-29 21:49:25 +0300

    jd02_01 start task A

---
+++ src/by/it/kameisha/jd02_01/Timer.java
@@ -0,0 +1,15 @@
+package by.it.kameisha.jd02_01;
+
+public class Timer {
+    
+    private Timer() {
+    }
+
+    public static void sleep(int timeout) {
+        try {
+            Thread.sleep(timeout / 100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
