f1348953fed4c31747dd7263cce38565d9e2f018
Kameisha <xim9052@gmail.com>
2022-05-29 12:51:53 +0300

    jd02_01 start task A

--- src/by/it/kameisha/jd02_01/Customer.java
+++ src/by/it/kameisha/jd02_01/Customer.java
@@ -1,4 +1,16 @@
 package by.it.kameisha.jd02_01;
 
 public class Customer {
+    private static long currentId = System.currentTimeMillis();
+    public final String name;
+    public final long id;
+    public Customer(long number) {
+        id = ++currentId;
+        name = "Customer №"+number;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
