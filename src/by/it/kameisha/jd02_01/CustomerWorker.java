1bccabe8c03c40ef0ab6800ce6137b8594c3aa49
Kameisha <xim9052@gmail.com>
2022-05-29 12:45:47 +0300

    jd02_01 start task A

--- src/by/it/kameisha/jd02_01/CustomerWorker.java
+++ src/by/it/kameisha/jd02_01/CustomerWorker.java
@@ -1,4 +1,28 @@
 package by.it.kameisha.jd02_01;
 
-public class CustomerWorker {
+public class CustomerWorker implements CustomerAction, ShoppingCardAction {
+    @Override
+    public void enteredStore() {
+
+    }
+
+    @Override
+    public Good chooseGood() {
+        return null;
+    }
+
+    @Override
+    public void goOut() {
+
+    }
+
+    @Override
+    public void takeCart() {
+
+    }
+
+    @Override
+    public int putToCart(Good good) {
+        return 0;
+    }
 }
--- src/by/it/kameisha/jd02_01/Runner.java
+++ src/by/it/kameisha/jd02_01/Runner.java
@@ -1,6 +1,6 @@
 package by.it.kameisha.jd02_01;
 
-public class ConsoleRunner {
+public class Runner {
     public static void main(String[] args) {
 
     }
