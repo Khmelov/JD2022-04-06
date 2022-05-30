1a53e0bb65e9fe4e65740270e1a3dfbbe7d7a893
Arseni Hlaz <mafusaik@gmail.com>
2022-05-30 20:29:51 +0300

    jd01-15 A, B done

--- src/by/it/arsenihlaz/jd01_15/TaskB.java
+++ src/by/it/arsenihlaz/jd01_15/TaskB.java
@@ -3,61 +3,25 @@ package by.it.arsenihlaz.jd01_15;
 import by.it.arsenihlaz.jd01_14.Util;
 
 import java.io.*;
-//fdgdfgfdgdgfg
-/*
-sdfsdfsf
-       */
 
 public class TaskB {
 
     public static void main(String[] args) {
-
         String pathFile = Util.getPath(TaskB.class, "TaskB.java");
         File thisFile = new File(pathFile);
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(thisFile))) {
+        try (RandomAccessFile fileReader = new RandomAccessFile(thisFile, "r")) {
+            fileReader.seek(0);
             printText(fileReader, stringBuilder);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(stringBuilder);
+        String txtFile = Util.getPath(TaskB.class, "TaskB.txt");
+        printTxtFile(stringBuilder, txtFile);
     }
 
-    private static void readText(BufferedReader fileReader, StringBuilder stringBuilder) throws IOException {
-        while (fileReader.ready()) {
-            int symbol = fileReader.read();
-            if ((char) symbol == '\n') {
-                printText(fileReader, stringBuilder);
-            }
-        }
-    }
-
-    private static void readText2(BufferedReader fileReader, StringBuilder stringBuilder) throws IOException {
-        while (fileReader.ready()) {
-            int symbol = fileReader.read();
-            if ((char) symbol == '*') {
-                checkText2(fileReader, stringBuilder);
-            }
-        }
-    }
-
-    private static void checkText2(BufferedReader fileReader, StringBuilder stringBuilder) throws IOException {
-        if (fileReader.read() == '/') {
-            printText(fileReader, stringBuilder);
-        } else readText2(fileReader, stringBuilder);
-    }
-
-    private static void checkText(BufferedReader fileReader, StringBuilder stringBuilder) throws IOException {
-        if (fileReader.read() == '/') {
-            readText(fileReader, stringBuilder);
-        } else printText(fileReader, stringBuilder);
-        if (fileReader.read() == '*') {
-            readText2(fileReader, stringBuilder);
-        } else printText(fileReader, stringBuilder);
-    }
-
-    private static void printText(BufferedReader fileReader, StringBuilder stringBuilder) throws IOException {
-        while (fileReader.ready()) {
+    private static void printText(RandomAccessFile fileReader, StringBuilder stringBuilder) throws IOException {
+        while (fileReader.getFilePointer() != fileReader.length()) {
             int symbol = fileReader.read();
             stringBuilder.append((char) symbol);
             if ((char) symbol == '/') {
@@ -65,4 +29,58 @@ public class TaskB {
             }
         }
     }
+
+    private static void checkText(RandomAccessFile fileReader, StringBuilder stringBuilder) throws IOException {
+        int nextSymbol = fileReader.read();
+        if (nextSymbol == '/') {
+            fileReader.seek(fileReader.getFilePointer()-1);
+            stringBuilder.deleteCharAt(stringBuilder.length()-1);
+            readText(fileReader, stringBuilder);
+        } else if (nextSymbol == '*') {
+            fileReader.seek(fileReader.getFilePointer()-1);
+            stringBuilder.deleteCharAt(stringBuilder.length()-1);
+            readText2(fileReader, stringBuilder);
+        } else fileReader.seek(fileReader.getFilePointer()-1);
+            printText(fileReader, stringBuilder);
+    }
+
+    private static void readText(RandomAccessFile fileReader, StringBuilder stringBuilder) throws IOException {
+        while (fileReader.getFilePointer() != fileReader.length()) {
+            int nextSymbol = fileReader.read();
+            if (nextSymbol == '\n') {
+                fileReader.seek(fileReader.getFilePointer()-1);
+                printText(fileReader, stringBuilder);
+            }
+        }
+    }
+
+    private static void checkText2(RandomAccessFile fileReader, StringBuilder stringBuilder) throws IOException {
+        if (fileReader.read() == '/') {
+            printText(fileReader, stringBuilder);
+        } else
+            readText2(fileReader, stringBuilder);
+    }
+
+    private static void readText2(RandomAccessFile fileReader, StringBuilder stringBuilder) throws IOException {
+        while (fileReader.getFilePointer() != fileReader.length()) {
+            int nextSymbol = fileReader.read();
+            if (nextSymbol == '*') {
+                checkText2(fileReader, stringBuilder);
+            }
+        }
+    }
+
+    private static void printTxtFile(StringBuilder stringBuilder, String txtFile) {
+        try (PrintWriter out = new PrintWriter(txtFile)) {
+            out.println(stringBuilder);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+/*
+sdfsdfsf
+sdfdsfyjyj
+       */
+//fdgdfgfdgdgfg
+
 }
