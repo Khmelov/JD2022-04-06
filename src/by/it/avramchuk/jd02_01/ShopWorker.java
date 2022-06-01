8024867ba78ed3841f2ad0b8c4f2714044e7580f
Аврамчук Сергей <dvizenets@mail.ru>
2022-06-01 12:16:57 +0300

    jd02_01 complete

--- src/by/it/avramchuk/jd02_01/ShopWorker.java
+++ src/by/it/avramchuk/jd02_01/ShopWorker.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ShopWorker extends Thread{
     private final Shop shop;
-
+    private int number=0;
     public ShopWorker(Shop shop) {
         this.shop = shop;
     }
@@ -17,8 +17,8 @@ public class ShopWorker extends Thread{
 
     @Override
     public void run() {
+
         System.out.println(shop+" opened");
-        int number=0;
         List<CustomerWorker> customerWorkerList = new ArrayList<>();
         for (int min = 0; min < 2; min++) {
             for (int sec = 0; sec < 60; sec++) {
@@ -28,29 +28,14 @@ public class ShopWorker extends Thread{
                         currentCount++;
                     }
                 }
-                int countCustomerPerSecond=0;
-                if(sec<30) {
-                    countCustomerPerSecond = (sec+15)-currentCount;
-                }else if(currentCount<=(40+(30-sec))){
-                    countCustomerPerSecond = (40+(30-sec))-currentCount;
-                }
+                int countCustomerPerSecond = needToAdd(currentCount, sec);
                 for (int i = 0; i < countCustomerPerSecond; i++) {
-
-                    int whoIsCustomer = RandomGenerator.get(100);
-                    Customer customer;
-                    if (whoIsCustomer <= 25) {
-                        customer = new Pensioner(++number);
-                    } else if (whoIsCustomer >= 50) {
-                        customer = new Student(++number);
-                    } else {
-                        customer = new Customer(++number);
-                    }
-
+                    Customer customer = defineCustomer();
                     CustomerWorker customerWorker = new CustomerWorker(customer, shop);
                     customerWorker.start();
                     customerWorkerList.add(customerWorker);
                 }
-                System.out.println("\n at " + sec + " second " + currentCount + "\n");
+                System.out.println("\n at " + sec + " second " + currentCount + " customers in the store\n" );
                 Timer.sleep(1000);
             }
         }
@@ -62,5 +47,29 @@ public class ShopWorker extends Thread{
             }
         }
         System.out.println(shop+" closed");
+        System.out.println("\n total visitors: "+ number);
+    }
+
+    public Customer defineCustomer(){
+        Customer customer;
+        int whoIsCustomer = RandomGenerator.get(100);
+        if (whoIsCustomer <= 25) {
+            customer = new Pensioner(++number);
+        } else if (whoIsCustomer >= 50) {
+            customer = new Student(++number);
+        } else {
+            customer = new Customer(++number);
+        }
+        return customer;
+    }
+
+    public int needToAdd (int currentCount, int sec){
+        int countCustomerPerSecond=0;
+        if(sec<30) {
+            countCustomerPerSecond = (sec+15)-currentCount;
+        }else if(currentCount<=(40+(30-sec))){
+            countCustomerPerSecond = (40+(30-sec))-currentCount;
+        }
+        return countCustomerPerSecond;
     }
 }
