4e61a6529593a10317fe6e40405301f79675bcaa
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-07 20:33:10 +0300

    JD02_02 Store class: -add synchronization -refactored

--- src/by/it/marchenko/jd02_02/models/Store.java
+++ src/by/it/marchenko/jd02_02/models/Store.java
@@ -1,23 +1,39 @@
 package by.it.marchenko.jd02_02.models;
 
+import by.it.marchenko.jd02_02.constants.StoreConstant;
+import by.it.marchenko.jd02_02.services.ManagerWorker;
+
 import static by.it.marchenko.jd02_02.constants.StoreConstant.STORE;
 
 public class Store {
-
-    private static int startID = 1;
+    private static int startID = StoreConstant.STORE_START_ID;
 
     private final int id;
     private final String name;
-    private final Manager manager;
+    private final StoreQueue storeQueue;
+    private final ManagerWorker managerWorker;
 
-    public Store(String name, Manager manager) {
+    public Store(String name, StoreQueue storeQueue, ManagerWorker managerWorker) {
         this.name = name;
-        this.manager = manager;
+        this.managerWorker = managerWorker;
         this.id = startID++;
+        this.storeQueue = storeQueue;
     }
 
     public Manager getManager() {
-        return manager;
+        return managerWorker.getManager();
+    }
+
+    public ManagerWorker getManagerWorker() {
+        return managerWorker;
+    }
+
+    public StoreQueue getStoreQueue() {
+        return storeQueue;
+    }
+
+    public Object getMonitor() {
+        return storeQueue;
     }
 
     @Override
