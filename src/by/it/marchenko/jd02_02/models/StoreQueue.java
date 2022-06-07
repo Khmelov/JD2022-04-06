86d571a6de4d3faf3bcfa7dffda155584de57e72
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-07 20:41:14 +0300

    JD02_02 StoreQueue class implemented

---
+++ src/by/it/marchenko/jd02_02/models/StoreQueue.java
@@ -0,0 +1,44 @@
+package by.it.marchenko.jd02_02.models;
+
+import by.it.marchenko.jd02_02.constants.StoreConstant;
+import by.it.marchenko.jd02_02.interfaces.StoreQueueAction;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class StoreQueue implements StoreQueueAction {
+    private final Queue<Customer> storeQueue;
+
+    public StoreQueue() {
+        this.storeQueue = new ArrayDeque<>();
+    }
+
+    @Override
+    public int getSize() {
+        synchronized (storeQueue) {
+            return storeQueue.size();
+        }
+    }
+
+    @Override
+    public boolean add(Customer customer) {
+        synchronized (storeQueue) {
+            return storeQueue.add(customer);
+        }
+    }
+
+    @Override
+    public Customer remove() {
+        synchronized (storeQueue) {
+            return storeQueue.poll();
+        }
+    }
+
+    @Override
+    public int getExpectedCashierCount(boolean mode) {
+        synchronized (storeQueue) {
+            int expectedCashierCount = (int) Math.ceil(storeQueue.size() / (double) StoreConstant.CUSTOMER_PER_CASHIER);
+            return mode ? StoreConstant.DEFAULT_CASHIER_COUNT : Math.min(expectedCashierCount, StoreConstant.MAX_CASHIER_COUNT);
+        }
+    }
+}
