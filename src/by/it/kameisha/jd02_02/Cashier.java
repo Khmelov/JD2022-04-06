a073cb960644a2bd91ab18819d96fbb00597e7cc
Kameisha <xim9052@gmail.com>
2022-06-08 23:09:20 +0300

    jd02_02 start task A jd02_01 complete A,B,C

---
+++ src/by/it/kameisha/jd02_02/Cashier.java
@@ -0,0 +1,14 @@
+package by.it.kameisha.jd02_02;
+
+public class Cashier {
+    private final String name;
+
+    public Cashier(int number) {
+        this.name = "Cashier№"+number;
+    }
+
+    @Override
+    public String toString() {
+        return "\t" + name;
+    }
+}
---
+++ src/by/it/kameisha/jd02_02/CashierWorker.java
@@ -0,0 +1,8 @@
+package by.it.kameisha.jd02_02;
+
+public class CashierWorker implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
