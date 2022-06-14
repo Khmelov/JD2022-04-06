7dca390e6768b8ad0979e19772910c973ee50316
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-15 01:19:07 +0300

    JD02-02 Store class refactored

--- src/by/it/marchenko/jd02_02/models/Store.java
+++ src/by/it/marchenko/jd02_02/models/Store.java
@@ -1,12 +1,11 @@
 package by.it.marchenko.jd02_02.models;
 
-import by.it.marchenko.jd02_02.constants.StoreConstant;
 import by.it.marchenko.jd02_02.services.ManagerWorker;
 
-import static by.it.marchenko.jd02_02.constants.StoreConstant.STORE;
+import static by.it.marchenko.jd02_02.constants.StoreConstant.*;
 
 public class Store {
-    private static int startID = StoreConstant.STORE_START_ID;
+    private static int startID = STORE_START_ID;
 
     private final int id;
     private final String name;
@@ -14,9 +13,9 @@ public class Store {
     private final ManagerWorker managerWorker;
 
     public Store(String name, StoreQueue storeQueue, ManagerWorker managerWorker) {
+        this.id = startID++;
         this.name = name;
         this.managerWorker = managerWorker;
-        this.id = startID++;
         this.storeQueue = storeQueue;
     }
 
