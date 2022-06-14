0f28662ec6c3f5608ba476df0e6b115cff4d901d
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-15 01:37:00 +0300

    JD02-02 PriceListRepo class refactored

--- src/by/it/marchenko/jd02_02/repository/PriceListRepo.java
+++ src/by/it/marchenko/jd02_02/repository/PriceListRepo.java
@@ -10,16 +10,12 @@ public class PriceListRepo implements PriceListAction {
     private final Map<Integer, Double> priceList = new HashMap<>();
 
     @Override
-    public void setPrice(int id, double price) {
-        priceList.put(id, price);
-    }
-
-    @Override
     public double getPrice(int id) {
         return priceList.get(id);
     }
 
-    public Map<Integer, Double> getPriceList() {
-        return new HashMap<>(priceList);
+    @Override
+    public void setPrice(int id, double price) {
+        priceList.put(id, price);
     }
 }
