8ec004f5da31c5193caf812143f622e0810f7a5d
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-15 00:43:20 +0300

    JD02-02 Printer class refactored

--- src/by/it/marchenko/jd02_02/Printer.java
+++ src/by/it/marchenko/jd02_02/Printer.java
@@ -3,16 +3,12 @@ package by.it.marchenko.jd02_02;
 import java.io.PrintStream;
 
 public class Printer extends PrintStream {
-    //private PrintStream out;
 
     public Printer(PrintStream out) {
-        //    this.out=out;
         super(out);
     }
 
     public Printer() {
-        //   this.out=System.out;
         super(System.out);
-        System.out.println();
     }
 }
