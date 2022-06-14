f8151871c523e366747b67f30d5222d54ee440c4
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-15 01:10:08 +0300

    JD02-02 ManagerWorker class refactored

--- src/by/it/marchenko/jd02_02/services/ManagerWorker.java
+++ src/by/it/marchenko/jd02_02/services/ManagerWorker.java
@@ -10,11 +10,24 @@ public class ManagerWorker implements ManagerAction {
     private volatile int totalCustomerCount;
     private volatile int notServicedCustomerCount;
 
-//    private volatile int currentCashierCount = 0;
-
     public ManagerWorker(Manager manager) {
         this.manager = manager;
         this.plan = manager.getPlan();
+    }
+
+    @Override
+    public Manager getManager() {
+        return manager;
+    }
+
+    @Override
+    public int getTotalCustomerCount() {
+        return totalCustomerCount;
+    }
+
+    @Override
+    public int getNotServicedCustomerCount() {
+        return notServicedCustomerCount;
     }
 
     @Override
@@ -25,25 +38,6 @@ public class ManagerWorker implements ManagerAction {
     }
 
     @Override
-    public int getTotalCustomerCount() {
-        return totalCustomerCount;
-    }
-
-    @Override
-    public boolean storeOpened() {
-        return totalCustomerCount != plan;
-    }
-
-    @Override
-    public boolean storeClosed() {
-        return totalCustomerCount == plan;
-    }
-
-    public Manager getManager() {
-        return manager;
-    }
-
-    @Override
     public void increaseNotServicedCustomerCount() {
         synchronized (manager) {
             notServicedCustomerCount++;
@@ -51,23 +45,7 @@ public class ManagerWorker implements ManagerAction {
     }
 
     @Override
-    public int getNotServicedCustomerCount() {
-        return notServicedCustomerCount;
+    public boolean storeOpened() {
+        return totalCustomerCount != plan;
     }
-/*
-    public int getCurrentCashierCount() {
-        synchronized (this) {
-            return currentCashierCount;
-        }
-    }
-
-    public void setCurrentCashierCount(int currentCashierCount) {
-        synchronized (this) {
-            this.currentCashierCount = currentCashierCount;
-        }
-    }
-
-
- */
-
 }
