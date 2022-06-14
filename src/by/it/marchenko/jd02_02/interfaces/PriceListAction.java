7ef0086ad25d94769fc79813fb21c87ea720c3ad
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-15 01:36:33 +0300

    JD02-02 PriceListAction interface refactored

--- src/by/it/marchenko/jd02_02/interfaces/PriceListAction.java
+++ src/by/it/marchenko/jd02_02/interfaces/PriceListAction.java
@@ -1,8 +1,8 @@
 package by.it.marchenko.jd02_02.interfaces;
 
 public interface PriceListAction {
-    void setPrice(int id, double price);
-
     double getPrice(int id);
+
+    void setPrice(int id, double price);
 
 }
