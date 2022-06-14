4e8b52954f7686280e08088a8ef15c0dfb08df08
Delarez <92412420+Delarez@users.noreply.github.com>
2022-06-15 01:33:54 +0300

    JD02-02 GoodRepo class refactored

--- src/by/it/marchenko/jd02_02/repository/GoodRepo.java
+++ src/by/it/marchenko/jd02_02/repository/GoodRepo.java
@@ -9,13 +9,12 @@ public class GoodRepo implements GoodActions {
     private final Map<Integer, String> goodNames = new HashMap<>();
 
     @Override
-    public void addName(int goodID, String goodName) {
-        goodNames.put(goodID, goodName);
-        goodNames.get(goodID);
+    public String getName(int goodID) {
+        return goodNames.get(goodID);
     }
 
     @Override
-    public String getName(int goodID) {
-        return goodNames.get(goodID);
+    public void setName(int goodID, String goodName) {
+        goodNames.put(goodID, goodName);
     }
 }
