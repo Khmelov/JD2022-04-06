1ac3bd5e83565a6150cf43be495cecaaa428aa70
Оксана Рагач <oksanaragach@gmail.com>
2022-06-17 16:16:09 +0300

    jd02-02 Task B start

--- src/by/it/ragach/jd02_02/entity/Cashier.java
+++ src/by/it/ragach/jd02_02/entity/Cashier.java
@@ -3,6 +3,8 @@ package by.it.ragach.jd02_02.entity;
 public class Cashier {
     public final String name;
 
+    public double revenue = 0.0;
+
     public Cashier(int number) {
         this.name = "Cashier №" + number;
     }
