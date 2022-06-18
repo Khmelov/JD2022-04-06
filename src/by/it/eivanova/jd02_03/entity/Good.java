edacc2b0a4debdd993bdfb5e8e826809eeb04c12
Alena Ivanova <alena.ivanova.sk@gmail.com>
2022-06-18 17:34:43 +0300

    jd02_02 start Ivanova

---
+++ src/by/it/eivanova/jd02_03/entity/Good.java
@@ -0,0 +1,21 @@
+package by.it.eivanova.jd02_03.entity;
+
+public class Good {
+
+    private final String name;
+
+    public Good(String name) {
+        this.name = name;
+    }
+
+    public Good() {
+        name = "unknown good";
+    }
+
+    @Override
+    public String toString() {
+        return "Good{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/by/it/eivanova/jd02_03/interfaces/CustomerAction.java
@@ -0,0 +1,13 @@
+package by.it.eivanova.jd02_03.interfaces;
+
+import by.it.eivanova.jd02_03.entity.Good;
+
+public interface CustomerAction {
+    void enteredStore(); //вошел в магазин (мгновенно)
+
+    Good chooseGood(); //выбрал товар (от 0,5 до 2 секунд)
+
+    void goToQueue();
+
+    void goOut(); //отправился на выход(мгновенно)
+}
