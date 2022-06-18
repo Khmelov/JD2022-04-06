6228a65619730d8ba728590ccb9d9b08b90a0e25
Alena Ivanova <alena.ivanova.sk@gmail.com>
2022-06-18 17:24:23 +0300

    jd02_02 start Ivanova

--- src/by/it/eivanova/jd02_02/ConsoleRunner.java
+++ src/by/it/eivanova/jd02_02/ConsoleRunner.java
@@ -1,9 +1,9 @@
 package by.it.eivanova.jd02_02;
 
-import by.it._classwork_.jd02_02.entity.Manager;
-import by.it._classwork_.jd02_02.entity.Queue;
-import by.it._classwork_.jd02_02.entity.Shop;
-import by.it._classwork_.jd02_02.service.ShopWorker;
+import by.it.eivanova.jd02_02.entity.Manager;
+import by.it.eivanova.jd02_02.entity.Queue;
+import by.it.eivanova.jd02_02.entity.Shop;
+import by.it.eivanova.jd02_02.service.ShopWorker;
 
 public class ConsoleRunner {
 
@@ -15,11 +15,7 @@ public class ConsoleRunner {
         Shop shop = new Shop("EuroMarket", euroMarketQueue, manager);
         ShopWorker shopWorker = new ShopWorker(shop);
         shopWorker.start();
-        //  }
-//        Queue sosediQueue = new Queue();
-//        Shop shop2 = new Shop("Sosedi", sosediQueue);
-//        ShopWorker shopWorker2 = new ShopWorker(shop2);
-//        shopWorker2.start();
+
     }
 
 }
