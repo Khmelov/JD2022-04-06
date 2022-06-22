521bb1bcba44621c8def721ec5b4789596d24116
Оксана Рагач <oksanaragach@gmail.com>
2022-06-22 16:35:07 +0300

    jd02_04 start

---
+++ src/by/it/ragach/calc/Scalar.java
@@ -0,0 +1,71 @@
+package by.it.ragach.calc;
+
+class Scalar extends Var {
+
+    private final double value;
+
+    Scalar(double value) {
+        this.value = value;
+    }
+
+    Scalar(Scalar scalar) {
+        this.value = scalar.value;
+    }
+
+    Scalar(String stringValue) {
+        value = Double.parseDouble(stringValue);
+
+    }
+
+
+    @Override
+    public Var add(Var other)throws CalcException {
+        //1+2
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value + otherScalar.value);
+        } else {
+            return other.add(this);
+        }
+    }
+
+    @Override
+    public Var sub(Var other)throws CalcException {
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value - otherScalar.value);
+        } else {
+            return other.add(this.mul(new Scalar(-1)));
+        }
+
+    }
+
+    @Override
+    public Var mul(Var other) throws CalcException{
+        if (other instanceof Scalar otherScalar) {
+            return new Scalar(this.value * otherScalar.value);
+        } else {
+            return other.add(this);
+        }
+    }
+
+    @Override
+    public Var div(Var other) throws CalcException {
+        if (other instanceof Scalar otherScalar) {
+            if (otherScalar.value == 0) {
+                throw new CalcException("division by zero %s/%s", this, otherScalar);
+            }
+            return new Scalar(this.value / otherScalar.value);
+        } else {
+            return super.div(other);
+        }
+
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+}
---
+++ src/by/it/ragach/calc/Var.java
@@ -0,0 +1,42 @@
+package by.it.ragach.calc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+abstract class Var implements Operation {
+
+    private static final Map<String,Var> vars = new HashMap<>();
+
+
+
+
+    @Override
+      public String toString() {
+          return "abstract Var{}";
+      }
+
+      @Override
+      public Var add(Var other) throws CalcException {
+         throw new CalcException("Incorrect operation %s + %s%n", this,other);
+
+      }
+
+      @Override
+      public Var sub(Var other)throws CalcException {
+          throw new CalcException("Incorrect operation %s - %s%n", this,other);
+
+      }
+
+      @Override
+      public Var mul(Var other)throws CalcException {
+         throw new CalcException("Incorrect operation %s * %s%n", this,other);
+
+      }
+
+      @Override
+      public Var div(Var other) throws CalcException {
+          throw new  CalcException("Incorrect operation %s / %s%n", this,other);
+
+      }
+  }
