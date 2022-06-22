b12a227e3a46c22d4f98b05a4e36ee72b348aca2
AnnaZhehulovich <aneto44ka@mail.tu>
2022-06-23 00:38:20 +0300

    Task jd02-05 started A

---
+++ src/by/it/annazhegulovich/jd02_05/Runner.java
@@ -0,0 +1,19 @@
+package by.it.annazhegulovich.jd02_05;
+
+import by.it.annazhegulovich.jd02_05.txtFiles.answer;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Runner {
+    public static void main(String[] args) {
+        ResMan resMan = ResMan.INSTANSE;
+        Locale locale = args.length == 2 ? new Locale(args[0], args[1]) : Locale.ENGLISH;
+        resMan.setLocale(locale);
+        System.out.println(resMan.get(massage.WELCOME));
+        System.out.println(resMan.get(massage.QUESTION1));
+        System.out.println(resMan.get(massage.QUESTION2));
+        System.out.println(resMan.get(answer.QUESTION1));
+        System.out.println(resMan.get(answer.QUESTION2));
+    }
+}
---
+++ src/by/it/annazhegulovich/jd02_05/txtFiles/answer.java
@@ -0,0 +1,7 @@
+package by.it.annazhegulovich.jd02_05.txtFiles;
+
+public interface answer {
+    String QUESTION1 = "answer.question1";
+    String QUESTION2 = "answer.question2";
+
+}
