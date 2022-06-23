0888ea909138b1f10aa64d7e759ab232a89306a9
Anton Smirnov <watashinodenwadesu@gmail.com>
2022-06-23 17:17:08 +0300

    jd02-05 Task A native2Ascii converter done.

---
+++ src/by/it/smirnov/jd02_05/Native2Ascii.java
@@ -0,0 +1,70 @@
+package by.it.smirnov.jd02_05;
+
+import java.io.*;
+import java.util.StringJoiner;
+
+public class Native2Ascii {
+
+    public static final String FILE_NAME_INPUT = "txtfiles%stext_%s.txt";
+    public static final String FILE_NAME_OUTPUT = "language%stext_%s.properties";
+    private static final String PREFIX = "\\u";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            convertFile(args[i]);
+        }
+    }
+
+    private static void convertFile(String lang) {
+        String fileNameIn = String.format(FILE_NAME_INPUT, File.separator, lang);
+        String fileNameOut = String.format(FILE_NAME_OUTPUT, File.separator, lang);
+        String pathIn = PathGetter.getPath(Native2Ascii.class, fileNameIn);
+        String pathOut = PathGetter.getPath(Native2Ascii.class, fileNameOut);
+        File converted = new File(pathOut);
+
+        try (var reader = new BufferedReader(new FileReader(pathIn));
+        var writer = new BufferedWriter (new FileWriter(pathOut, true))) {
+            var joiner = new StringJoiner("\n");
+            converted.createNewFile();
+            while (reader.ready()) {
+                String line = reader.readLine();
+                line = native2Ascii(line);
+                joiner.add(line);
+            }
+            writer.write(joiner.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String native2Ascii(String line) {
+        char[] chars = line.toCharArray();
+        var sb = new StringBuilder();
+        for (char aChar : chars) {
+            sb.append(char2Ascii(aChar));
+        }
+        return sb.toString();
+    }
+
+    private static String char2Ascii(char c) {
+        if (c > 255) {
+            var sb = new StringBuilder();
+            sb.append(PREFIX);
+            int code = (c >> 8);
+            String temp = Integer.toHexString(code);
+            if (temp.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(temp);
+            code = (c & 0xFF);
+            temp = Integer.toHexString(code);
+            if (temp.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(temp);
+            return sb.toString();
+        } else {
+            return Character.toString(c);
+        }
+    }
+}
