e9891f4eae5071fc5807a18c72d56c4666dee7d5
Аврамчук Сергей <dvizenets@mail.ru>
2022-06-24 14:43:00 +0300

    jd02_05 level A

---
+++ src/by/it/avramchuk/jd02_05/constants/Message.java
@@ -0,0 +1,6 @@
+package by.it.avramchuk.jd02_05.constants;
+
+public interface Message {
+    String QUESTION="message.question";
+    String WELCOME="message.welcome";
+}
---
+++ src/by/it/avramchuk/jd02_05/constants/User.java
@@ -0,0 +1,6 @@
+package by.it.avramchuk.jd02_05.constants;
+
+public interface User {
+    String FIRST_NAME="user.firstname";
+    String LAST_NAME="user.lastname";
+}
