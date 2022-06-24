21c201aa8a1b9b476ce0dee17b4eebe72815cdae
Аврамчук Сергей <dvizenets@mail.ru>
2022-06-24 16:49:44 +0300

    jd02_05 level B

--- src/by/it/avramchuk/jd02_05/Runner.java
+++ src/by/it/avramchuk/jd02_05/Runner.java
@@ -3,7 +3,10 @@ package by.it.avramchuk.jd02_05;
 import by.it.avramchuk.jd02_05.constants.Message;
 import by.it.avramchuk.jd02_05.constants.User;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
@@ -13,10 +16,23 @@ public class Runner {
                 ? new Locale(args[0], args[1])
                 : Locale.ENGLISH;
         resMan.setLocale(locale);
+
+        System.out.println(resMan.get(Message.START_MESSAGE));
+        Scanner scanner = new Scanner(System.in);
+        String inPut = scanner.nextLine();
+        locale = new Locale(inPut);
+        resMan.setLocale(locale);
+
         System.out.println(resMan.get(Message.WELCOME));
         System.out.println(resMan.get(Message.QUESTION));
         System.out.println(resMan.get(User.FIRST_NAME));
         System.out.println(resMan.get(User.LAST_NAME));
+
+        ZonedDateTime zdt = ZonedDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm dd MMMM yyyy", locale);
+        String time = dtf.format(zdt);
+        System.out.println(time);
+
 
     }
 }
--- src/by/it/avramchuk/jd02_05/constants/Message.java
+++ src/by/it/avramchuk/jd02_05/constants/Message.java
@@ -3,4 +3,5 @@ package by.it.avramchuk.jd02_05.constants;
 public interface Message {
     String QUESTION="message.question";
     String WELCOME="message.welcome";
+    String START_MESSAGE= "message.start";
 }
