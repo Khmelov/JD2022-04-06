4e916f8de97af83689e2da257f73d20118afee69
Anton Smirnov <watashinodenwadesu@gmail.com>
2022-06-25 13:06:55 +0300

    jd02-06 Task A complete.

--- src/by/it/smirnov/jd02_06/Logger.java
+++ src/by/it/smirnov/jd02_06/Logger.java
@@ -1,21 +1,21 @@
-package by.it._classwork_.jd02_06;
-
-import by.it._classwork_.jd01_14.Util;
+package by.it.smirnov.jd02_06;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Objects;
 
+import static by.it.smirnov.jd02_06.PathGetter.getPath;
+
 public class Logger {
+    private static volatile Logger logger;
 
     public static final String LOG_TXT = "log.txt";
-    private static volatile Logger logger;
 
     private Logger() {
     }
 
-    public Logger getInstance() {
+    public static Logger getInstance() {
         Logger localLogger = logger;
         if (Objects.isNull(localLogger)) {
             synchronized (Logger.class) {
@@ -30,11 +30,9 @@ public class Logger {
     }
 
     public void log(String message) {
-        String path = Util.getPath(Logger.class, LOG_TXT);
-        try (
-                PrintWriter printWriter = new PrintWriter(new FileWriter(path, true))
-        ) {
-            printWriter.println(message);
+        String path = getPath(Logger.class, LOG_TXT);
+        try (PrintWriter writer = new PrintWriter(new FileWriter(path, true))) {
+writer.println(message);
         } catch (IOException e) {
             throw new AssertionError(e);
         }
