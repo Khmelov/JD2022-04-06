b8173d129ca45cfc6085f620a3519cb1a139a9f3
Alena Ivanova <alena.ivanova.sk@gmail.com>
2022-06-25 22:52:06 +0300

    jd02_01 finished Ivanova

--- src/by/it/eivanova/jd02_01/ShopWorker.java
+++ src/by/it/eivanova/jd02_01/ShopWorker.java
@@ -14,12 +14,23 @@ public class ShopWorker extends Thread {
     public void run() {
         System.out.println(shop + " opened");
         int number = 0;
-        List<CustomerWorker> customerWorkerList=new ArrayList<>();
+        int minGoods = 2;
+        int maxGoods = 5;
+        List<CustomerWorker> customerWorkerList = new ArrayList<>();
         for (int time = 0; time < 120; time++) {
             int countCustomerPerSecond = RandomGenerator.get(2);
             for (int i = 0; i < countCustomerPerSecond; i++) {
-                Customer customer = new Customer(++number);
-                CustomerWorker customerWorker = new CustomerWorker(customer, shop);
+                Customer customer;
+                if ((number + 1)  % 4 == 0) {
+                    customer = new Customer("Pensioner", ++number);
+                } else if ((number + 1) % 2 == 0) {
+                    customer = new Customer("Student", ++number);
+                    minGoods = 0;
+                    maxGoods = 2;
+                } else {
+                    customer = new Customer("Default", ++number);
+                }
+                CustomerWorker customerWorker = new CustomerWorker(customer, shop, minGoods, maxGoods);
                 customerWorker.start();
                 customerWorkerList.add(customerWorker);
                 Timer.sleep(1000);
@@ -35,6 +46,4 @@ public class ShopWorker extends Thread {
         }
         System.out.println(shop + " closed");
     }
-
-
 }
