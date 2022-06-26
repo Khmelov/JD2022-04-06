d157a3347ed6df4ad96f8a817d976664d151feef
Аврамчук Сергей <dvizenets@mail.ru>
2022-06-26 10:28:31 +0300

    jd02_06 started

--- src/by/it/avramchuk/calc/ConsoleRunner.java
+++ src/by/it/avramchuk/calc/ConsoleRunner.java
@@ -4,7 +4,6 @@ import by.it.avramchuk.calc.constants.Message;
 import by.it.avramchuk.calc.entity.Var;
 import by.it.avramchuk.calc.exception.CalcException;
 import by.it.avramchuk.calc.interfaces.Repository;
-import by.it.avramchuk.calc.repository.PersistentRepository;
 import by.it.avramchuk.calc.repository.VarMapRepository;
 import by.it.avramchuk.calc.service.Parser;
 import by.it.avramchuk.calc.service.Printer;
@@ -17,30 +16,30 @@ import java.util.Scanner;
 public class ConsoleRunner {
 
     public static final String COMMAND_END = "end";
-    public static final String MESSEGE_START = "App started";
-    public static final String MESSEGE_STOP = "App finished";
+    public static final String EN = "en";
+    public static final String RU = "ru";
+    public static final String BE = "be";
 
     public static void main(String[] args) {
         Printer printer = new Printer(System.out);
         Repository repository = new VarMapRepository();
         VarCreator varCreator = new VarCreator(repository);
-        Locale locale = Locale.getDefault();
         ResMan resMan = ResMan.INSTANCE;
 
         Parser parser = new Parser(repository, varCreator);
-        Scanner  scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println(resMan.get(Message.START_MESSAGE));
-        while (scanner.hasNext()){
+        while (scanner.hasNext()) {
             String expression = scanner.nextLine();
-            if (expression.equalsIgnoreCase("en")||
-            expression.equalsIgnoreCase("ru")||
-            expression.equalsIgnoreCase("be")){
-                locale = new Locale(expression.toLowerCase());
+            if (expression.equalsIgnoreCase(EN) ||
+                    expression.equalsIgnoreCase(RU) ||
+                    expression.equalsIgnoreCase(BE)) {
+                Locale locale = new Locale(expression.toLowerCase());
                 resMan.setLocale(locale);
                 System.out.println(resMan.get(Message.LANGUAGE_MESSAGE));
                 continue;
             }
-            if (expression.equals(COMMAND_END)){
+            if (expression.equals(COMMAND_END)) {
                 break;
             } else {
                 try {
---
+++ src/by/it/avramchuk/jd02_06/Logger.java
@@ -0,0 +1,4 @@
+package by.it.avramchuk.jd02_06;
+
+public class Logger {
+}
