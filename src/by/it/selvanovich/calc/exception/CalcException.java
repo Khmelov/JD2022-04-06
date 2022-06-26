a17c7ba2bf302595e8b5d0cfd833db6f9f376205
Danila Selvanovich <lineconnor@yandex.ru>
2022-06-26 03:03:03 +0300

    jd02-04 Complete C

---
+++ src/by/it/selvanovich/calc/exception/CalcException.java
@@ -0,0 +1,21 @@
+package by.it.selvanovich.calc.exception;
+
+public class CalcException extends Exception{
+
+
+    public CalcException(String message) {
+        super(message);
+    }
+
+    public CalcException(String format, Object... parameters) {
+        this(String.format(format, parameters));
+    }
+
+    public CalcException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+
+
+}
