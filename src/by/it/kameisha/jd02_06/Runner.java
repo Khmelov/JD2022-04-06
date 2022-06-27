938a0f6787e3f6f2ba5231ccc71aeb7f1a466701
Kameisha <xim9052@gmail.com>
2022-06-27 23:32:01 +0300

    jd02_06 start B         complete A jd02_05 complete A,B,C jd02_04 complete A,B,C

--- src/by/it/kameisha/jd02_06/Runner.java
+++ src/by/it/kameisha/jd02_06/Runner.java
@@ -4,6 +4,8 @@ public class Runner {
     public static void main(String[] args) {
         Logger logger = Logger.getInstance();
         logger.log("Hello");
-
+        for (int i = 0; i < 10; i++) {
+            new Worker("Worker №" + i).start();
+        }
     }
 }
