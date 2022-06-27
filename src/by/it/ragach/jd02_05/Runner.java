93d8a2d7dfaddb7040eb192beb4f93040c405001
Oksana Ragach <oksanaragach@gmail.com>
2022-06-27 14:03:34 +0300

    jd_02_05 Task B start

--- src/by/it/ragach/jd02_05/Runner.java
+++ src/by/it/ragach/jd02_05/Runner.java
@@ -3,8 +3,11 @@ package by.it.ragach.jd02_05;
 import by.it.ragach.jd02_05.constants.Message;
 import by.it.ragach.jd02_05.constants.User;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
@@ -15,9 +18,23 @@ public class Runner {
                 : Locale.ENGLISH;
 
         resMan.setLocale(locale);
+
+        System.out.println(resMan.get(Message.START));
+        Scanner scanner = new Scanner(System.in);
+        String inPut = scanner.nextLine();
+        locale = new Locale(inPut);
+        resMan.setLocale(locale);
+
         System.out.println(resMan.get(Message.WELCOME));
         System.out.println(resMan.get(Message.QUESTION));
         System.out.println(resMan.get(User.FIRST_NAME));
         System.out.println(resMan.get(User.LAST_NAME));
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm dd MMMM yyyy", locale);
+        String time = dateTimeFormatter.format(zonedDateTime);
+        System.out.println(time);
+
+
     }
 }
--- src/by/it/ragach/jd02_05/txtfiles/convertor.bat
+++ src/by/it/ragach/jd02_05/txtfiles/convertor.bat
@@ -1,4 +1,4 @@
-set path=D:\Java\jdk-8\bin
+set path=C:\jdk-8\bin
 native2ascii -encoding utf-8 text.txt ..\language\text.properties
 native2ascii -encoding utf-8 text_be.txt ..\language\text_be.properties
 native2ascii -encoding utf-8 text_ru.txt ..\language\text_ru.properties
