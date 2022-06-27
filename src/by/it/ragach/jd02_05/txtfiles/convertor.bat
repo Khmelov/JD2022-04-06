78f97f837f8539c23ff3e063416f237733fa5573
Оксана Рагач <oksanaragach@gmail.com>
2022-06-27 15:20:30 +0300

    jd02_05 TaskB completed

--- src/by/it/ragach/jd02_05/txtfiles/convertor.bat
+++ src/by/it/ragach/jd02_05/txtfiles/convertor.bat
@@ -1,7 +1,8 @@
-set path=C:\jdk-8\bin
+set path=D:\Java\jdk-8\bin
 native2ascii -encoding utf-8 text.txt ..\language\text.properties
 native2ascii -encoding utf-8 text_be.txt ..\language\text_be.properties
 native2ascii -encoding utf-8 text_ru.txt ..\language\text_ru.properties
-native2ascii -encoding utf-8 text_en.txt ..\language\text_en.properties
+native2ascii -encoding utf-8 text_ru.txt ..\language\text_en.properties
+
 
 
