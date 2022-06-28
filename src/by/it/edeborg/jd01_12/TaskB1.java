a499aca95c1a010b180da6ba393bde94d3c3c4be
Egor Edeborg <lexusxxxrock@gmail.com>
2022-06-28 19:13:37 +0300

    i have made jd01_11-15

---
+++ src/by/it/edeborg/jd01_12/TaskB1.java
@@ -0,0 +1,36 @@
+package by.it.edeborg.jd01_12;
+
+
+import java.util.*;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        Map<String, Integer> wordToCount = new HashMap<>();
+        while (scanner.hasNext()) {
+            String value = scanner.nextLine();
+            for (String word : value.split("[^a-zA-Z']+")) {
+                if (!word.equals("end")) {
+                    if (wordToCount.containsKey(word)) {
+                        wordToCount.put(word, wordToCount.get(word) + 1);
+                    } else {
+                        wordToCount.put(word, 1);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            Set<Map.Entry<String, Integer>> setWord = wordToCount.entrySet();
+            Iterator<Map.Entry<String, Integer>> iWord = setWord.iterator();
+            while (iWord.hasNext()) {
+                Map.Entry<String, Integer> me = iWord.next();
+                System.out.println(me.getKey() + "=" + me.getValue());
+
+            }
+        }
+    }
+}
